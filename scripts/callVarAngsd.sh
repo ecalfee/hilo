323b986123e9556da34016bdbf49039a3e9cf51d
ecalfee <erincalfee@gmail.com>
2018-05-29 15:21:03 -0700

    draft script to call variants using ANGSD based on samtools genotype likelihoods for pass1 samples

--- scripts/callVarAngsd.sh
+++ scripts/callVarAngsd.sh
@@ -15,7 +15,7 @@ set –o errexit
 set –o nounset
 
 # load angsd
-module load angsd ****
+module load angsd
 
 # make directory to store output (if doesn't yet exist)
 mkdir -p var_sites
@@ -29,10 +29,10 @@ echo "calling variants using ANGSD on BAMS for hilo $SLURM_ARRAY_TASK_ID"
 # (1) For each chromosome individually, find variant sites
 
 angsd -out var_sites/chr$SLURM_ARRAY_TASK_ID \
--r REGION \
--doMajorMinor 4 -ref REFERENCE.fa \
--bam pass1_bam.all.list \
--GL 1 \
+-r $SLURM_ARRAY_TASK_ID \
+-doMajorMinor 4 -ref /group/jrigrp/Share/assemblies/Zea_mays.AGPv4.dna.chr.fa \
+-bam data/pass1_bam.all.list \
+-GL 1 -doGlf 2 \
 -baq 1 -minMapQ 30 -minQ 20 \
 -doMaf 4 -minMaf 0.1 \
 -rmTriallelic 1e-5 \
@@ -43,18 +43,10 @@ angsd -out var_sites/chr$SLURM_ARRAY_TASK_ID \
 # -doMajorMinor 4: pre-specify major allele from reference genome and infer minor allele from genotype likelihood
 # -bam list of bams to include (all newly sequenced allopatric mex. and sympatric mexicana & maize pops)
 # -GL 1: use samtools genotype likelihood method
-# -baq 1 -minMapQ 30 -minQ 20: calculate BAQ adjusted quality score and filter out sites with low quality or low mapping
+# -doGlf 2: output beagle likelihood file
+# -baq 1 -minMapQ 30 -minQ 20: calculate BAQ adjusted quality score and filter out sites with low mapping quality or base/BAQ quality
 # -doMaf 4 : use frequency from genotype probabilities to calculate minor allele freq
 # an alternative would be to use AlleleCounts method (-doMaf 8)
 # -minMaf 0.1: and then do a cutoff to only include variant sites with >10% diff. from reference allele
 # -rmTriallelic 1e-5: remove high likelihood triallelic sites (p=value< .00001)
 # -minInd 50: only keep sites with information (at least one read) from 50 individuals
-
-#angsd -GL 1 -ref /group/jrigrp/Share/assemblies/Zea_mays.AGPv4.dna.chr.fa -bam testBAM.list -r 1:10000-20000 -out testANGSD
-	-> angsd version: 0.911 (htslib: 1.2.1-263-g2b2239a) build(Mar 11 2016 12:48:20)
-	-> Reading fasta: /group/jrigrp/Share/assemblies/Zea_mays.AGPv4.dna.chr.fa
-	-> fai index file: '/group/jrigrp/Share/assemblies/Zea_mays.AGPv4.dna.chr.fa.fai' looks older than corresponding fastafile: '/group/jrigrp/Share/assemblies/Zea_mays.AGPv4.dna.chr.fa'.
-	-> Please reindex fasta file
-# I need to index the reference, e.g.:
-samtools faidx <ref.fa>
-# I can either make a copy onto my local drive an index it or I can index the copy in the Share folder
