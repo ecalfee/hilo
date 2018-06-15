9fd6838ff9a740f390dae8d5a1e6dd07a749eddf
Erin Calfee <erincalfee@gmail.com>
2018-06-15 16:48:24 -0700

    still a problem with SNPs out of order

--- scripts/pruneFixedcM.py
+++ scripts/pruneFixedcM.py
@@ -56,7 +56,7 @@ with gzip.open(fileOut + ".gz", mode = "wt") as fileSNP:
                         #print("at SNP " + str(chr2)+ ":" + str(pos2) + "cM") 
                         if chr2 == chr1 and (pos2 - pos1) < minL: # if same chromosome and too close, skip SNP
                             if pos1 is not None and pos2 < pos1:
-                                raise ValueError("Positions must be in order on each chromosome! Error at Chr" + str(chr2) + ":" + str(pos2) + " coming before Chr" + str(chr1) + ":" + str(pos1))
+                                raise ValueError("Positions must be in order on each chromosome! Error at Chr" + str(chr1) + ":" + str(pos1) + " coming before Chr" + str(chr2) + ":" + str(pos2) + " at bp-position " + str(posBP2) + " file " + fileIn)
                             pass 
                         else: # include if on diff chrom or not too close
                             writerSNP.writerow(row) # print line (SNP included)
