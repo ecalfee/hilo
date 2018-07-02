545671dded39c9fa2f5fa1ff8ba3572e40f2d86a
Erin Calfee <erincalfee@gmail.com>
2018-07-01 21:49:23 -0700

    updated angsd version; for Fst first calculating SAF by chromosome

--- scripts/calcSAFAngsd_byChr.sh
+++ scripts/calcSAFAngsd_byChr.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -l
 #SBATCH --partition=bigmemm
-#SBATCH -D /home/ecalfee/hilo/scripts
-#SBATCH -J SAFAngsd
-#SBATCH -o /home/ecalfee/hilo/slurm-log/SAFAngsd_%j_%A_%a.out
-#SBATCH -t 65:00:00
-#SBATCH -n 10
-#SBATCH --mem=75G
-#SBATCH --array=18-31,33-35,360-363,365-374,1000,2000,3000
+#SBATCH -D /home/ecalfee/hilo/data
+#SBATCH -J SAFByChr
+#SBATCH -o /home/ecalfee/hilo/slurm-log/SAFByChr_%j_%A_%a.out
+#SBATCH -t 64:00:00
+#SBATCH --mem=25G
+#SBATCH -n 3
+#SBATCH --array=1-10
 
+# POP variable needs to be set when calling the script
+# e.g. sbatch --export=POP=18 calcSAFAngsd_byChr.sh
 # pops >=1000 are the larger groupings of maize sympatric, mexicana sympatric and mexicana allopatric inds
 
 # general bash script settings to make sure if any errors in the pipeline fail
@@ -16,31 +18,30 @@ set –o pipefail
 set –o errexit
 set –o nounset
 
-# calculate SAF by chromosome
-sbatch --expor=POP=$SLURM_ARRAY_TASK_ID calcSAFAngsd_byChr.sh
-
-# go to folder with results by chromosome
-cd ../data/SFS/pass1/pop$SLURM_ARRAY_TASK_ID
-# and concatenate results together
-realSFS cat chr1.saf.idx chr2.saf.idx chr3.saf.idx chr4.saf.idx chr5.saf.idx chr6.saf.idx chr7.saf.idx chr8.saf.idx chr9.saf.idx chr10.saf.idx -outname ../pop$SLURM_ARRAY_TASK_ID
+# rather than load module, use local module of angsd & print version
+# angsd --version
 
 # make directory to store output (if doesn't yet exist)
-mkdir -p SFS/pass1/
+mkdir -p SFS/pass1/pop$POP
 
-echo "finding allele frequencies for pop$SLURM_ARRAY_TASK_ID"
+echo "finding allele frequencies for pop$POP, chr$SLURM_ARRAY_TASK_ID"
 # steps:
 # (0) Start with filtered BAM files and reference genome
 # (1) Use all sites to estimate site allele frequency
-angsd -out SFS/pass1/pop$SLURM_ARRAY_TASK_ID \
+angsd -out SFS/pass1/pop$POP/chr$SLURM_ARRAY_TASK_ID \
 -anc /group/jrigrp/Share/assemblies/Zea_mays.AGPv4.dna.chr.fa -fold 1 \
--bam pass1_bam_pops/pop$SLURM_ARRAY_TASK_ID.list \
+-r $SLURM_ARRAY_TASK_ID \
+-bam pass1_bam_pops/pop$POP.list \
 -remove_bads 1 -minMapQ 30 \
--GL 1 -dosaf 1
+-GL 1 -dosaf 1 \
+-P 3
 echo "done with SAF"
 
 # settings:
+# -r sets a region of the chromosome to do the SAF calculation -- here split by chromosomes
 # -doSaf 1: Calculate the Site allele frequency likelihood based on individual genotype likelihoods assuming HWE
 # -anc reference_genome with -fold 1 calculates a folded SFS (in lieue of having an ancestral state/genome)
 # -remove_bads removes reads with flags like duplicates & -minMapQ 30: filter out sites with low mapping quality
 # -bam list of bams to include
 # -GL 1: use samtools genotype likelihood method
+# -P sets number of angsd threads to use (but multithreading doesn't apply to IO read/write tasks)
