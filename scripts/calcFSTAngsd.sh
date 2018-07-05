4219d6b59fa14fcefe10634b2806129f26cab9db
ecalfee <erincalfee@gmail.com>
2018-07-05 14:54:32 -0700

    scripts to calculate Fst

--- scripts/calcFSTAngsd.sh
+++ scripts/calcFSTAngsd.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -l
 #SBATCH --partition=med
 #SBATCH -D /home/ecalfee/hilo/data
-#SBATCH -J 2DSFS
-#SBATCH -o /home/ecalfee/hilo/slurm-log/2DSFS_%j_%A_%a.out
-#SBATCH -t 4:00:00
+#SBATCH -J FST
+#SBATCH -o /home/ecalfee/hilo/slurm-log/FST_%j_%A_%a.out
+#SBATCH -t 1:00:00
 #SBATCH --mem=1G
-#SBATCH -n 6
 
 # to run: sbatch --export=POP1=18,POP2=19,DIR=SAF/pass1/N1000.L100.regions calc2DSFSAngsd.sh
 # note: need to set POP1 and POP2 and directory variables
@@ -19,7 +18,9 @@ set –o pipefail
 set –o errexit
 set –o nounset
 
-# calculate pairwise 2D SFS
-realSFS $DIR/pop$POP1.saf.idx $DIR/pop$POP2.saf.idx -P 6 > $DIR/pop$POP1.pop$POP2.sfs
+# calculate FST (-whichFst 1 indicates Hudson/Bhatia2013 estimator for Fst)
+realSFS fst index $DIR/pop$POP1.saf.idx $DIR/pop$POP2.saf.idx -sfs $DIR/pop$POP1.pop$POP2.sfs -whichFst 1 -fstout $DIR/pop$POP1.pop$POP2
+# future analyses should rely on 'weighted' Fst (i.e. the ratio of expectations across loci).
+realSFS fst stats $DIR/pop$POP1.pop$POP2.fst.idx > $DIR/pop$POP1.pop$POP2.fst.stats
 
-echo "done calculating pairwise 2D SFS for pop"$POP1" and pop"$POP2
+echo "done calculating pairwise FST for pop"$POP1" and pop"$POP2
