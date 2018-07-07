d57de6ea3068ae7e50fdb8e7e9a0a2323df6017e
ecalfee <erincalfee@gmail.com>
2018-07-06 17:53:47 -0700

    scripts to calculate depth of coverage for hilo passing filters

---
+++ scripts/calcDepthCovRegions.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+#SBATCH --partition=bigmemm
+#SBATCH -D /home/ecalfee/hilo/data
+#SBATCH -J calcDepthRegions
+#SBATCH -o /home/ecalfee/hilo/slurm-log/calcDepthRegions_%j_%A_%a.out
+#SBATCH -t 36:00:00
+#SBATCH --mem=10G
+#SBATCH --export=REGIONS_FILE=N1000.L100.regions
+
+# general bash script settings to make sure if any errors in the pipeline fail
+# then it’s a ‘fail’ and it passes all errors to exit and allows no unset variables
+set –o pipefail
+set –o errexit
+set –o nounset
+
+# load angsd
+module load angsd
+
+# make directory to store output (if doesn't yet exist)
+mkdir -p depthCov/pass1
+
+# apply filtering with SAMtools & PICARD
+echo "calculating depth using ANGSD on BAMS for hilo "$REGIONS_FILE
+# calculate depth only including bases meeting minQ >20
+angsd -out depthCov/pass1/$REGIONS_FILE.Q20 \
+-rf refMaize/random_regions/$REGIONS_FILE \
+-bam pass1_bam.all.list \
+-remove_bads 1 \
+-minMapQ 30 -minQ 20 \
+-doDepth 1 
+# calculate depth for bases meeting any base quality
+angsd -out depthCov/pass1/$REGIONS_FILE \
+-rf refMaize/random_regions/$REGIONS_FILE \
+-bam pass1_bam.all.list \
+-remove_bads 1 \
+-minMapQ 30 \
+-doDepth 1 
+echo "done calculating depth using ANGSD on BAMS for hilo "$REGIONS_FILE
---
+++ scripts/calcDepthCovSites.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+#SBATCH --partition=bigmemm
+#SBATCH -D /home/ecalfee/hilo/data
+#SBATCH -J calcDepthRegions
+#SBATCH -o /home/ecalfee/hilo/slurm-log/calcDepthRegions_%j_%A_%a.out
+#SBATCH -t 48:00:00
+#SBATCH --mem=20G
+#SBATCH --export=SITES_FILE=all.positions,SITES_DIR=var_sites/pass1/pruned_positions
+
+# general bash script settings to make sure if any errors in the pipeline fail
+# then it’s a ‘fail’ and it passes all errors to exit and allows no unset variables
+set –o pipefail
+set –o errexit
+set –o nounset
+
+# load angsd
+module load angsd
+
+# make directory to store output (if doesn't yet exist)
+mkdir -p depthCov/pass1
+
+# apply filtering with SAMtools & PICARD
+echo "calculating depth using ANGSD on BAMS for hilo "$REGIONS_FILE
+# calculate depth only including bases meeting minQ >20
+angsd -out depthCov/pass1/$SITES_FILE.Q20 \
+-sites $SITES_DIR/$SITES_FILE \
+-bam pass1_bam.all.list \
+-remove_bads 1 \
+-minMapQ 30 -minQ 20 \
+-doDepth 1 
+# calculate depth for bases meeting any base quality
+angsd -out depthCov/pass1/$SITES_FILE \
+-sites $SITES_DIR/$SITES_FILE \
+-bam pass1_bam.all.list \
+-remove_bads 1 \
+-minMapQ 30 \
+-doDepth 1 
+echo "done calculating depth using ANGSD on BAMS for hilo "$REGIONS_FILE
