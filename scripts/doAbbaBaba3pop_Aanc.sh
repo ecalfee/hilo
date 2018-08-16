85a856410a80703de10390dbe2d2b13ebadb1b0f
ecalfee <erincalfee@gmail.com>
2018-08-15 20:12:00 -0700

    abba baba not running:

---
+++ scripts/doAbbaBaba3pop_Aanc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+#SBATCH --partition=med
+#SBATCH -D /home/ecalfee/hilo/data
+#SBATCH -J ABBA_BABA
+#SBATCH -o /home/ecalfee/hilo/slurm-log/ABBA_BABA_%j_%A_%a.out
+#SBATCH -t 12:00:00
+#SBATCH --mem=8G
+
+# this script uses allopatric maize as the outgroup .. this will be underpowered to detect
+# introgression between sympatric maize and mexicana if there is also gene flow to allopatric mexicana
+OUT_FILE=SAF/pass1/N1000.L100.regions
+BAM_FILE=AbbaBaba/doAbbaBaba3pop_maize_bam.list
+SIZE_FILE=AbbaBaba/doAbbaBaba3pop_maize.sizeFile
+REGIONS=refMaize/random_regions/N1000.L100.regions
+
+# general bash script settings to make sure if any errors in the pipeline fail
+# then it’s a ‘fail’ and it passes all errors to exit and allows no unset variables
+set –o pipefail
+set –o errexit
+set –o nounset
+
+echo "now calculating ABBA-BABA"
+
+angsd -doAbbababa2 \
+-Aanc 1 -rf $REGIONS -bam $BAM_FILE \
+-sizeFile $SIZE_FILE -out $OUT_FILE
+
+#-blockSize 100 \
+
+echo "done calculating ABBA-BABA"
+# v2 uses all individuals
+# -useLast 1 means make last bam file the 'outgroup'
+# alternative without an outgroup is to do -Aanc which makes all ancestral alleles an A arbitrarily (and is otherwise an f3 test ?)
+# -rf for regions file
+# block size I just made the same as my blocks in the genome..should be fine (?) .. they shouldn't have LD between them
