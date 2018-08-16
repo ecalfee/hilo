ea5e952ec8903c85c0ce5bcc95857d550c682429
Erin Calfee <erincalfee@gmail.com>
2018-08-16 10:52:31 -0700

    fixed abbababa scripts

--- scripts/doAbbaBaba3pop_Aanc.sh
+++ scripts/doAbbaBaba3pop_Aanc.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
 #SBATCH --partition=med
 #SBATCH -D /home/ecalfee/hilo/data
-#SBATCH -J ABBA_BABA
-#SBATCH -o /home/ecalfee/hilo/slurm-log/ABBA_BABA_%j_%A_%a.out
+#SBATCH -J ABBA_Aanc
+#SBATCH -o /home/ecalfee/hilo/slurm-log/ABBA_BABA_Aanc_%j_%A_%a.out
 #SBATCH -t 12:00:00
 #SBATCH --mem=8G
+#SBATCH --export=PREFIX=3pop_alloMaize_sympMaize_alloMex_bam
 
-# this script uses allopatric maize as the outgroup .. this will be underpowered to detect
-# introgression between sympatric maize and mexicana if there is also gene flow to allopatric mexicana
-OUT_FILE=SAF/pass1/N1000.L100.regions
-BAM_FILE=AbbaBaba/doAbbaBaba3pop_maize_bam.list
-SIZE_FILE=AbbaBaba/doAbbaBaba3pop_maize.sizeFile
+# this script uses an arbitrary A as the ancestral allele/outgroup
+OUT_FILE=AbbaBaba/$PREFIX.regions.abbababa
+BAM_FILE=AbbaBaba/$PREFIX.list
+SIZE_FILE=AbbaBaba/$PREFIX.sizeFile
 REGIONS=refMaize/random_regions/N1000.L100.regions
 
 # general bash script settings to make sure if any errors in the pipeline fail
@@ -21,15 +21,19 @@ set –o nounset
 
 echo "now calculating ABBA-BABA"
 
-angsd -doAbbababa2 \
--Aanc 1 -rf $REGIONS -bam $BAM_FILE \
--sizeFile $SIZE_FILE -out $OUT_FILE
-
+angsd -doAbbababa2 1 \
+-Aanc 1 \
+-rf $REGIONS \
+-bam $BAM_FILE \
+-sizeFile $SIZE_FILE \
+-out $OUT_FILE \
+-minQ 20 \
+-minMapQ 30 \
+-doCounts 1
 #-blockSize 100 \
 
 echo "done calculating ABBA-BABA"
 # v2 uses all individuals
-# -useLast 1 means make last bam file the 'outgroup'
-# alternative without an outgroup is to do -Aanc which makes all ancestral alleles an A arbitrarily (and is otherwise an f3 test ?)
+# -Aanc makes all ancestral alleles an A arbitrarily (and is otherwise an f3 test ?)
 # -rf for regions file
 # block size I just made the same as my blocks in the genome..should be fine (?) .. they shouldn't have LD between them
--- scripts/doAbbaBaba4pop_maize.sh
+++ scripts/doAbbaBaba4pop_maize.sh
@@ -8,9 +8,9 @@
 
 # this script uses allopatric maize as the outgroup .. this will be underpowered to detect
 # introgression between sympatric maize and mexicana if there is also gene flow to allopatric mexicana
-OUT_FILE=SAF/pass1/N1000.L100.regions
-BAM_FILE=AbbaBaba/doAbbaBaba4pop_maize_bam.list
-SIZE_FILE=AbbaBaba/doAbbaBaba4pop_maize.sizeFile
+OUT_FILE=AbbaBaba/4pop_maize_bam.regions.abbababa
+BAM_FILE=AbbaBaba/4pop_maize_bam.list
+SIZE_FILE=AbbaBaba/4pop_maize_bam.sizeFile
 REGIONS=refMaize/random_regions/N1000.L100.regions
 # to run: sbatch --export=POP1=18,POP2=19,DIR=SAF/pass1/N1000.L100.regions calc2DSFSAngsd.sh
 
@@ -22,9 +22,15 @@ set –o nounset
 
 echo "now calculating ABBA-BABA"
 
-angsd -doAbbababa2 \
--useLast 1 -rf $REGIONS -bam $BAM_FILE \
--sizeFile $SIZE_FILE -out $OUT_FILE
+angsd -doAbbababa2 1 \
+-useLast 1 \
+-rf $REGIONS \
+-bam $BAM_FILE \
+-sizeFile $SIZE_FILE \
+-out $OUT_FILE \
+-minQ 20 \
+-minMapQ 30 \
+-doCounts 1
 
 #-blockSize 100 \
 
@@ -33,4 +39,6 @@ echo "done calculating ABBA-BABA"
 # -useLast 1 means make last bam file the 'outgroup'
 # alternative without an outgroup is to do -Aanc which makes all ancestral alleles an A arbitrarily (and is otherwise an f3 test ?)
 # -rf for regions file
+# set basic mapQ and base Quality filters
+# doing allele counts is required for abba-baba -doCounts 1
 # block size I just made the same as my blocks in the genome..should be fine (?) .. they shouldn't have LD between them
