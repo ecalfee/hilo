40e049f03499b0a853292456d834e5bd3be2a53c
Erin Calfee <erincalfee@gmail.com>
2018-08-28 11:05:23 -0700

    updated script to prune for minimum cM distance after filtering out uninformative sites

--- scripts/pruneFixedcM.py
+++ scripts/pruneFixedcM.py
@@ -1,43 +1,41 @@
-# this script prunes a set of SNPs, read from a gzipped tab deliminated file,
+# this script prunes a set of SNPs, read from a tab deliminated variant sites file,
 # so that no two are closer than L cM (RECOMBINATION DISTANCE -- to reduce LD)
 
-# to run: hilo/scripts/$ python3 pruneFixedcM.py 0.0001 55 0.1 ../data/TEST/10SNPS.prunedR.mafs ../data/TEST/10SNPS.mafs.gz ../data/TEST/10SNPS.B.mafs.gz
+# to run: hilo/scripts/$ python3 pruneFixedcM.py 0.0001 55 0.1 out_file in_file1 in_file2
 import sys
 import csv
-import gzip
 import pandas
 import calcMapPos # helper function to calculate map position in cM from bp pos
 
 
-# get input 
-minL = float(sys.argv[1]) # minimum length (in cM) between 2 SNPs that are kept 
+# get input
+minL = float(sys.argv[1]) # minimum length (in cM) between 2 SNPs that are kept
 minInd = int(sys.argv[2]) # minimum number of individuals with data
-minMAF = float(sys.argv[3]) # column number specifying 
-fileOut = sys.argv[4] # file/path to write results to (.gz will be added for gzip compressed)
-listIn = sys.argv[5:] # list of files/paths to read in (gzipped) 
+minMAF = float(sys.argv[3]) # column number specifying
+fileOut = sys.argv[4] # file/path to write results to as a new sites file .var.sites and .distM distance in Morgans file
+listIn = sys.argv[5:] # list of files/paths to read in (sites files, not gzipped)
 
-print("Pruning R: minL= " + str(minL) + "cM, minInd= " + str(minInd) + " minMAF= " + str(minMAF) + " fileOut= " + fileOut + " listIn= " + str(listIn))
+print("Pruning R: minL= " + str(minL) + "cM, " + " fileOut= " + fileOut + " listIn= " + str(listIn))
 
-# set values
-# chromo	position	major	minor	phat	nInd
+# sites input file should have the following format:
+# chromo	position	major	minor
 colChr = 0 # column number of SNP chromosome/scaffold/LG. (index starts at zero!)
 colPosBP = 1 # column number of SNP position column (index starts at zero!)
-colMAF = 4 # column number specifying minimum allele freq. of SNPs to include
-colNInd = 5 # column number specifying number of individuals with data to include a SNP
 
 # get linkage map
-rmapALL = pandas.read_csv("../data/linkage_map/ogut_fifthcM_map_agpv4.txt", sep = "\t", header = None, names = ["name", "marker", "pos_cM", "chrom", "pos_bp"])
+rmapALL = pandas.read_csv("../data/linkage_map/ogut_fifthcM_map_agpv4_INCLUDE.txt", sep = "\t", header = None, names = ["name", "marker", "pos_cM", "chrom", "pos_bp"])
 
 pos1 = None # starting position
 chr1 = None # starting chromosome
 # first output file has all included SNPs with original bp positions, and is gzipped
-with gzip.open(fileOut + ".gz", mode = "wt") as fileSNP:
+with open(fileOut + ".var.sites", mode = "wt") as fileSNP:
     writerSNP = csv.writer(fileSNP, delimiter = "\t")
-    # second output file has recombination position only for all included SNPs
-    with open(fileOut + ".rpos", mode = "w") as writerPOS:
+    # second output file has distance in Morgans between all included SNPs
+    # (where first SNP of any chromosome is set to 1, an arbitrary choice from ancestry_hmm)
+    with open(fileOut + ".distM", mode = "w") as writerPOS:
         # for each input file, find SNPs to include
         for fileIn in listIn:
-            with gzip.open(fileIn, mode = "rt") as fileRead:
+            with open(fileIn, mode = "rt") as fileRead:
                 reader = csv.reader(fileRead, delimiter = "\t")
                 for row in reader:
                     try:
@@ -45,28 +43,23 @@ with gzip.open(fileOut + ".gz", mode = "wt") as fileSNP:
                     except ValueError: # if row[colPosBP] isn't an integer, skip and go to next line because current line is a header
                         print("skipping header line: " + str(row))
                         continue
-                    if float(row[colMAF]) < minMAF or int(row[colNInd]) < minInd:
-                        #print("no pass, MAF=" + row[colMAF] + ", NInd=" + row[colNInd])
-                        pass # doesn't meet minimum criteria
-                    else: # SNP passes minimum criteria
-                        # get new chrom
-                        chr2 = int(row[colChr])
-                        # get new recombination position 
-                        pos2 = calcMapPos.calcMapPos(chrom = chr2, pos = posBP2, rmap = rmapALL)
-                        #print("at SNP " + str(chr2)+ ":" + str(pos2) + "cM") 
-                        if chr2 == chr1 and (pos2 - pos1) < minL: # if same chromosome and too close, skip SNP
-                            if pos1 is not None and pos2 < pos1:
-                                raise ValueError("Positions must be in order on each chromosome! Error at Chr" + str(chr2) + ":" + str(pos2) + "at bp-position " + str(postBP2) + " should not come after Chr" + str(chr1) + ":" + str(pos1) + " at bp-position " + str(posBP1) + " file " + fileIn)
-                            pass 
-                        else: # include if on diff chrom or not too close
-                            writerSNP.writerow(row) # print line (SNP included)
-                            if chr2 != chr1: # first SNP included from a chromosome
-                                writerPOS.write(str(1) + "\n") # arbitrary but starts with difference = 1
-                            else: # print difference in Morgans
-                                writerPOS.write(str((pos2 - pos1)/100) + "\n") 
-                            chr1 = chr2 # update current chromosome
-                            pos1 = pos2 # update current position (cM)
-                            posBP1 = posBP2 # update current bp position
+
+                    # get new chrom
+                    chr2 = int(row[colChr])
+                    # get new recombination position
+                    pos2 = calcMapPos.calcMapPos(chrom = chr2, pos = posBP2, rmap = rmapALL)
+                    #print("at SNP " + str(chr2)+ ":" + str(pos2) + "cM")
+                    if chr2 == chr1 and (pos2 - pos1) < minL: # if same chromosome and too close, skip SNP
+                        if pos1 is not None and pos2 < pos1:
+                            raise ValueError("Positions must be in order on each chromosome! Error at Chr" + str(chr2) + ":" + str(pos2) + "at bp-position " + str(postBP2) + " should not come after Chr" + str(chr1) + ":" + str(pos1) + " at bp-position " + str(posBP1) + " file " + fileIn)
+                        pass
+                    else: # include if on diff chrom or not too close
+                        writerSNP.writerow(row) # print line (SNP included)
+                        if chr2 != chr1: # first SNP included from a chromosome
+                            writerPOS.write(str(1) + "\n") # arbitrary but starts with difference = 1
+                        else: # print difference in Morgans
+                            writerPOS.write(str((pos2 - pos1)/100) + "\n")
+                        chr1 = chr2 # update current chromosome
+                        pos1 = pos2 # update current position (cM)
+                        posBP1 = posBP2 # update current bp position
 print("Done pruning by fixed cM distance.")
-
-
--- scripts/pruneFixedcM.sh
+++ scripts/pruneFixedcM.sh
@@ -3,13 +3,13 @@
 #SBATCH -D /home/ecalfee/hilo/data/
 #SBATCH -J pruneFixcM
 #SBATCH -o /home/ecalfee/hilo/slurm-log/pruneFixcM_%j_%A_%a.out
-#SBATCH -t 16:00:00
+#SBATCH -t 10:00:00
 #SBATCH --mem=8G
 #SBATCH --array=1-10
-#SBATCH --export=MIN_IND=75
+#SBATCH --export=MIN_cM=.0001,dir_in="var_sites/merged_pass1_all_alloMaize4Low_16/filteredAlloMAFnInd",dir_out="var_sites/merged_pass1_all_alloMaize4Low_16/thinnedHMM"
 
-# the default for MIN_IND is set to 75 but can be overwritten when calling the script
-# e.g. sbatch --export=MIN_IND=100 pruneFixedcM.sh
+# this script prunes variant sites to fixed distance (.01cM ~10kb; .0001cM~.1kb)
+i=$SLURM_ARRAY_TASK_ID #chromosome
 
 # general bash script settings to make sure if any errors in the pipeline fail
 set –o pipefail
@@ -21,10 +21,11 @@ set –o nounset
 module load python3
 
 # make output directory
-mkdir -p var_sites/pass1/sympatric/pruned/minN$MIN_IND/
+mkdir -p $dir_out
 
-# prune to fixed distance .01cM (~10kb)
-python3 ../scripts/pruneFixedcM.py 0.01 $MIN_IND 0.1 var_sites/pass1/sympatric/pruned/minN$MIN_IND/thin01cM_chr$SLURM_ARRAY_TASK_ID.mafs \
-$(for i in $(awk -v chr=$SLURM_ARRAY_TASK_ID '$1 == chr {print $4}' refMaize/divide_50Mb/ALL_regions.list); do echo var_sites/pass1/sympatric/region_$i.mafs.gz; done)
+echo "pruning SNPs"
+python3 ../scripts/pruneFixedcM.py $MIN_cM $dir_out"/chr"$i \
+$(for i in $(awk -v chr=$i '$1 == chr {print $4}' refMaize/divide_50Mb/ALL_regions.list); \
+do echo $dir_in/region_$i.var.sites; done)
 # second line finds all regions associated with a specific chromosome (from file ALL_regions.list) and lists those mafs.gz files as input files to pruneFixedcM.py
-echo "done pruning SNPs for NGSadmix in hilo sympatric data only, minN="$MIN_IND" individuals, chr"$SLURM_ARRAY_TASK_ID
+echo "done pruning SNPs chr"$i
