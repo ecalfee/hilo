d3f685aff39fea48e3a479a3ec19d007c89086af
Erin Calfee <erincalfee@gmail.com>
2018-08-28 17:31:08 -0700

    new scripts to get allele and read counts for high and low coverage individuals

---
+++ scripts/countReadsRefAlt.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -l
+#SBATCH --partition=med
+#SBATCH -D /home/ecalfee/hilo/data
+#SBATCH -J countReads
+#SBATCH -o /home/ecalfee/hilo/slurm-log/countReadsRefAlt_%j_%A_%a.out
+#SBATCH -t 10:00:00
+#SBATCH --mem=8G
+#SBATCH --array=1-200
+#SBATCH --export=CHR=1
+
+# general bash script settings to make sure if any errors in the pipeline fail
+# then it’s a ‘fail’ and it passes all errors to exit and allows no unset variables
+set –o pipefail
+set –o errexit
+set –o nounset
+
+# this script takes in a VCF sites file (from high coverage maize, produced by plink)
+# and outputs a read counts file for the ref and alt allele for a low-coverage hilo individual
+
+# array is the hilo ID #
+id=$SLURM_ARRAY_TASK_ID
+VCF_POP="maize.allo.4Low16" # note the VCF is from plink (not ANGSD directly -- can't read that format)
+dir="var_sites/merged_pass1_all_alloMaize4Low_16/thinnedHMM/"
+
+# load modules
+# load GATK and dependencies
+module load R
+module load maven
+module load java
+module load GATK
+# -h to see help
+#java -Xmx6g -jar /share/apps/GATK-3.6/GenomeAnalysisTK.jar -T ASEReadCounter -h
+echo "counting ref and alt reads per individual in bams for chr"${CHR}" hilo id = "${id}
+java -Xmx7g -jar /share/apps/GATK-3.6/GenomeAnalysisTK.jar \
+-T ASEReadCounter \
+-R refMaize/AGPv4.fa \
+--sitesVCFFile ${dir}/${VCF_POP}_chr${i}.vcf \
+--out ${dir}/hilo_${id}_chr${CHR}.csv \
+--minBaseQuality 20 \
+--minMappingQuality 30 #\
+-I filtered_bam/hilo_${id}.sort.dedup.baq.rg.bam \
+-L $CHR
+
+echo "all done!"
---
+++ scripts/getVCFforAllo.sh
@@ -0,0 +1,66 @@
+#!/bin/bash -l
+#SBATCH --partition=bigmemm
+#SBATCH -D /home/ecalfee/hilo/data
+#SBATCH -J getVCF4Allo
+#SBATCH -o /home/ecalfee/hilo/slurm-log/getVCF4Allo_%j_%A_%a.out
+#SBATCH -t 12:00:00
+#SBATCH --mem=16G
+#SBATCH --array=1-10
+
+# array is the chromosome #
+POP="maize.allo.4Low16"
+i=$SLURM_ARRAY_TASK_ID
+sites_dir="var_sites/merged_pass1_all_alloMaize4Low_16/thinnedHMM/"
+
+# this script takes in a sites file and calls genotypes for allopatric individuals
+# from maize and mexicana, outputing a VCF
+
+# general bash script settings to make sure if any errors in the pipeline fail
+# then it’s a ‘fail’ and it passes all errors to exit and allows no unset variables
+set –o pipefail
+set –o errexit
+set –o nounset
+
+# don't load angsd, local copy is latest (v9.20)
+echo "calling genotypes and outputting a vcf chr"${i}" pop "${POP}
+# (1) make VCF in ANGSD
+angsd -out ${sites_dir}/${POP}_chr${i} \
+-r ${i} \
+-sites ${sites_dir}/chr${i}.var.sites \
+-bam pass1_bam_pops/${POP}.list \
+-remove_bads 1 \
+-minMapQ 30 \
+-minQ 20 \
+-doMajorMinor 3 \
+-doMaf 1 \
+-GL 1 \
+-doGeno -4 \
+-doPost 1 \
+-postCutoff 0.95 \
+-doVCF 1 \
+-P 1
+# output file = TEST/alloMaize_head_region_0.vcf.gz
+# (maybe should be more lenient (.9) posterior prob. for calling genotypes or later sample 1 read for lower coverage individuals ..not sure_
+# (and before doing this I think I want to filter for lower LD and high differentiation maize-mex in R)
+
+# settings:
+# -r specifies which region to work on
+# -remove_bads removes reads with flags like duplicates
+# -doMajorMinor 3: takes major & minor allele from sites file
+# -sites var.sites file has 4 tab separated columns: chrom pos major minor
+# -bam list of bams to include
+# -GL 1: use samtools genotype likelihood method
+# -doMaf 1 use fixed minor and major alleles. assumes HWE. alternative would be to use straight counts and no HWE assumption; -doMaf 8 with -doCounts 1
+# (MAF is used as the prior for the genotype posterior)
+# -minMapQ 30 -minQ 20: filter out sites with low mapping quality or base/BAQ quality
+# -doGeno -4: suppresses geno output (see ANGSD documentation plink page)
+# -doPost 1: calculate genotype posterior using MAF as prior
+# -postCutoff sets the posterior confidence below which no genotype is called (set instead as missing)
+# I do not set a separate cutoff for # of reads needed to call a genotype -geno_minDepth 4 (requires -doCounts)
+# -doVCF 1  outputs a vcf
+# (I pre-computed BAQ scores and replaced quality with minimum of BAQ/base quality,
+# so this is equivalend to -baq 2 option here)
+# -P n means use n threads/nodes for each angsd task (here task=chromosome; then merges threads within-chrom)
+
+
+echo "all done!"
