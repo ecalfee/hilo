70ebc82e293bec0118e085798d288d7d456cbc6a
Erin Calfee <erincalfee@gmail.com>
2019-02-27 00:32:07 -0800

    split homozygous tracts script into 2 scripts - 1 for mex, 1 for maize

---
+++ within_ancestry_diversity/get_homozyg_tracts_and_bams_maize.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH --partition=bigmemm
+#SBATCH -D /home/ecalfee/hilo/within_ancestry_diversity
+#SBATCH -J getMaize2
+#SBATCH -o /home/ecalfee/hilo/slurm-log/getHomozygTractsBamsMaize_%A_%a.out
+#SBATCH -t 2-00:00:00
+#SBATCH --mem=16G
+#SBATCH --array=1-200
+# to run: sbatch --export=DIR_BAMS=../filtered_bams/results/pass1,DIR_POST=../data/geno_lik/merged_pass1_all_alloMaize4Low_16/thinnedHMM/ancestry_hmm/output_noBoot get_homozyg_tracts_and_bams.sh
+
+# this script takes in a set of short tracts around sites with ancestry posteriors from ancestry_hmm
+# and the posteriors for an individual at those sites
+# then filters for sites with posterior probability > 0.8 of being homozygous
+# and merges tracts together with high probability homozygosity for maize ancestry
+# finally, filters bam files to only include aligned reads that overlap these homozygous ancestry tracts
+
+# general bash script settings to make sure if any errors in the pipeline fail
+# then it’s a ‘fail’ and it passes all errors to exit and allows no unset variables
+set –o pipefail
+set –o errexit
+set –o nounset
+
+# load modules
+module load bio # for bedtools and samtools
+
+i=$SLURM_ARRAY_TASK_ID
+SITES_FILE=results/input/var.sites.bed
+
+# make directory for output
+mkdir -p "results/input/maize2/tracts"
+mkdir -p "results/input/maize2/bams"
+
+# only run script if the posterior file can be found first
+if [ ! -f "$DIR_POST"/HILO"$i".posterior ]; then
+	echo "posterior does not exist: ${DIR_POST}/HILO${i}.posterior"
+else
+  # getting high posterior probability tracts homozygous for maize ancestry
+  echo "finding maize tracts"
+  pr -mt -s$'\t' "$SITES_FILE" <(tail -n +2 "$DIR_POST"/HILO"$i".posterior | cut -f3) | \
+  awk '$4 > 0.8 {print $0}' | bedtools merge -sorted -d 1 > results/input/maize2/tracts/HILO"$i".bed
+
+  # filtering bam for homozygous maize regions
+  echo "filtering bam for homozygous maize regions"
+  bedtools intersect -sorted -a "$DIR_BAMS"/HILO"$i".sort.dedup.baq.bam \
+  -b results/input/maize2/tracts/HILO"$i".bed > results/input/maize2/bams/HILO"$i".sort.dedup.baq.bam
+
+  echo "now indexing new bam!"
+  sleep 5s # because index needs to have a later timestamp
+  samtools index results/input/maize2/bams/HILO"$i".sort.dedup.baq.bam
+
+  echo "all done!"
+fi
--- within_ancestry_diversity/get_homozyg_tracts_and_bams_mexicana.sh
+++ within_ancestry_diversity/get_homozyg_tracts_and_bams_mexicana.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --partition=bigmemm
 #SBATCH -D /home/ecalfee/hilo/within_ancestry_diversity
-#SBATCH -J getHom
-#SBATCH -o /home/ecalfee/hilo/slurm-log/getHomozygTractsBams_%A_%a.out
+#SBATCH -J getMex2
+#SBATCH -o /home/ecalfee/hilo/slurm-log/getHomozygTractsBamsMex_%A_%a.out
 #SBATCH -t 2-00:00:00
 #SBATCH --mem=16G
 #SBATCH --array=1-200
-# to run: sbatch --export=DIR_BAM=../filtered_bams/results/pass1,DIR_POST=../data/geno_lik/merged_pass1_all_alloMaize4Low_16/thinnedHMM/ancestry_hmm/output_noBoot get_homozyg_tracts_and_bams.sh
+# to run: sbatch --export=DIR_BAMS=../filtered_bams/results/pass1,DIR_POST=../data/geno_lik/merged_pass1_all_alloMaize4Low_16/thinnedHMM/ancestry_hmm/output_noBoot get_homozyg_tracts_and_bams.sh
 
 # this script takes in a set of short tracts around sites with ancestry posteriors from ancestry_hmm
 # and the posteriors for an individual at those sites
 # then filters for sites with posterior probability > 0.8 of being homozygous
-# and merges tracts together with high probability homozygosity for maize or mexicana ancestry
+# and merges tracts together with high probability homozygosity formexicana ancestry
 # finally, filters bam files to only include aligned reads that overlap these homozygous ancestry tracts
 
 # general bash script settings to make sure if any errors in the pipeline fail
@@ -27,8 +27,6 @@ i=$SLURM_ARRAY_TASK_ID
 SITES_FILE=results/input/var.sites.bed
 
 # make directory for output
-mkdir -p "results/input/maize2/tracts"
-mkdir -p "results/input/maize2/bams"
 mkdir -p "results/input/mex2/tracts"
 mkdir -p "results/input/mex2/bams"
 
@@ -36,16 +34,6 @@ mkdir -p "results/input/mex2/bams"
 if [ ! -f "$DIR_POST"/HILO"$i".posterior ]; then
 	echo "posterior does not exist: ${DIR_POST}/HILO${i}.posterior"
 else
-  # getting high posterior probability tracts homozygous for maize ancestry
-  echo "finding maize tracts"
-  pr -mt -s$'\t' "$SITES_FILE" <(tail -n +2 "$DIR_POST"/HILO"$i".posterior | cut -f3) | \
-  awk '$4 > 0.8 {print $0}' | bedtools merge -sorted -d 1 > results/input/maize2/tracts/HILO"$i".bed
-
-  # filtering bam for homozygous maize regions
-  echo "filtering bam for homozygous maize regions"
-  bedtools intersect -sorted -a "$DIR_BAMS"/HILO"$i".sort.dedup.baq.bam \
-  -b results/input/maize2/tracts/HILO"$i".bed > results/input/maize2/bams/HILO"$i".sort.dedup.baq.bam
-
   # getting high posterior probability tracts homozygous for mexicana ancestry
   echo "finding mexicana tracts"
   pr -mt -s$'\t' "$SITES_FILE" <(tail -n +2 "$DIR_POST"/HILO"$i".posterior | cut -f5) | \
@@ -55,6 +43,10 @@ else
   echo "filtering bam for homozygous mexicana regions"
   bedtools intersect -sorted -a "$DIR_BAMS"/HILO"$i".sort.dedup.baq.bam \
   -b results/input/mex2/tracts/HILO"$i".bed > results/input/mex2/bams/HILO"$i".sort.dedup.baq.bam
+
+  echo "now indexing new bam!"
+  sleep 5s # because index needs to have a later timestamp
+  samtools index results/input/mex2/bams/HILO"$i".sort.dedup.baq.bam
 
   echo "all done!"
 fi
