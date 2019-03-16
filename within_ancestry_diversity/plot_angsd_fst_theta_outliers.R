cac0655fd50aebe7027b6839c83c9bdd7499d31a
ecalfee <erincalfee@gmail.com>
2019-03-15 18:11:47 -0700

    plots of pi within ancestry

--- within_ancestry_diversity/plot_angsd_fst_theta_outliers.R
+++ within_ancestry_diversity/plot_angsd_fst_theta_outliers.R
@@ -1,74 +1,88 @@
 library(dplyr)
 library(ggplot)
+library(reader)
 
 getTheta <- function(theta_file, pop, region){
-  d <- read.table(theta_file)[ , c(2:5,14)]
-  colnames(d) <- c("chr", "pos", "theta_W_sum", "theta_pi_sum", "n_bp")
-  d1 <- d %>% # scale thetas per bp not per region analyzed, based on total number of bp's with data
-    mutate(theta_W = theta_W_sum/n_bp) %>%
-    mutate(theta_pi = theta_pi_sum/n_bp) %>%
-    mutate(pop = pop) %>%
-    mutate(region = region)
+  if (file.exists(theta_file) && reader::file.nrow(theta_file) > 1){
+    d <- read.table(theta_file, sep = "\t")[ , c(2:5,14)]
+    colnames(d) <- c("chr", "pos", "theta_W_sum", "theta_pi_sum", "n_bp")
+    d1 <- d %>% # scale thetas per bp not per region analyzed, based on total number of bp's with data
+        mutate(theta_W = theta_W_sum/n_bp) %>%
+        mutate(theta_pi = theta_pi_sum/n_bp) %>%
+        mutate(pop = pop) %>%
+        mutate(region = region)
+    
+  }else{ # returns empty dataframe if no file exists (some regions some populations have no data)
+    d1 <- NULL
+    print(paste("warning: no theta file found for pop", pop, "region", region))
+  }
   return(d1)
 }
+
 getFst <- function(fst_file, pop, region){
-  d <- read.table(fst_file)
+  if (file.exists(theta_file)){
+  d <- read.table(fst_file, sep = "\t")
   colnames(d) <- c("fst_Hudson", "fst_avg_ratios")
   d1 <- d %>%
     mutate(pop = pop) %>%
     mutate(region = region)
+  }else{
+    d1 <- NULL
+    print(paste("warning: no fst file found for pop", pop, "region", region))
+  }
   return(d1)
 }
 
+#name = "outliers"
+#name = "outliers_100kb_flanking"
+#name = "outliers_mean_anc0.6"
+name = "outliers_mean_anc0.6_100kb_flanking"
 
+outliers <- read.table(paste0("../ZAnc/results/pass1_maize/", name, ".bed"), stringsAsFactors = F, header = F)
 
-exclude_slurm_tasks <- c(0,6) # errors running
-#outliers <- read.table("../ZAnc/results/pass1_maize/outliers.bed", stringsAsFactors = F, header = F)[-(exclude_slurm_tasks+1), ]
-
-#colnames(outliers) <- c("chr", "start", "end", "max_ZAnc", "max_pop_freq")
+colnames(outliers) <- c("chr", "start", "end", "max_ZAnc", "max_pop_freq")
 fst_pairs <- c("symp.maize-allo.mexicana", "symp.maize-symp.mexicana", "symp.mexicana-allo.mexicana")
-pops <- c("symp.maize", "symp.mexicana", "allo.mexicana") 
-#i = 1
-#outliers[i,]
-#dir <- paste0("results/piFst_outliers/mex2/chr", outliers[i, "chr"], "_", outliers[i,"start"], "-", outliers[i, "end"])
-regions <- read.table("results/piFst_outliers/mex2/regions.txt", stringsAsFactors = F, header = F)[-(exclude_slurm_tasks+1), ]
+
+pops <- unique(read.table("../data/pass1_ids.txt", stringsAsFactors = F, 
+                          header = T, sep = "\t")[ , c("popN", "zea", "symp_allo", "LOCALITY")]) %>%
+  mutate(group = paste(substr(symp_allo, 1, 4), zea, sep = ".")) %>%
+  mutate(pop = paste0("pop", popN))
+groups <- unique(pops$group)
+unique(pops$popN)
+pops_all <- sort(pops$pop)
+#pops_incl <- pops_all[c(1:15, 17, 22:25, 27, 29:30)] # some populations don't have proper files (maybe no homozygotes?)
+
+regions <- with(outliers, paste0("chr", chr, "_", start, "-", end))
+regions_incl <- regions[-c(1)] # missing Fst stats for this region -- skip for now
 
 thetas <- do.call(rbind,
                   lapply(regions, function(r) do.call(rbind,
-                  lapply(pops, function(pop) getTheta(file.path("results/piFst_outliers/mex2", r,
-                                                                paste0(pop, ".thetasAll.pestPG")), pop, region = r)))))
-
-
-
-#thetas <- do.call(rbind,
-#                  lapply(1:nrow(outliers), function(r) do.call(rbind,
-#                                                               lapply(pops, function(pop) 
-#                                                                 file.exists(file.path(paste0("results/piFst_outliers/mex2/chr", outliers[r, "chr"], "_", outliers[i,"start"], "-", outliers[i, "end"]),
-#                                                                                                             paste0(pop, ".thetasAll.pestPG")))))))
-
-
-
+                  lapply(groups, function(pop) getTheta(paste0("results/piFst_", name, "/mex2/", r, "/", pop, ".thetasAll.pestPG"), 
+                         pop = pop, region = r)))))
 
 
 ggplot(thetas, aes(x = pop, y = theta_pi, color = region)) +
   geom_point() + 
   geom_line(aes(group = region)) +
-  ggtitle("pi across groups for high mexicana ancestry ZAnc outlier regions")
-ggsave(filename = "pi_outliers.png", device = "png",
+  ggtitle(paste0("pi across groups for high mex ", name))
+ggsave(filename = paste0("plots/pi_groups_", name, ".png"), device = "png",
        plot = last_plot(), 
        width = 10, height = 10, units = "in",
        dpi = 300)
 
 
-FSTs <- do.call(rbind, # some excluded (file issues)
-                  lapply(regions[c(1:3,6:26)], function(r) do.call(rbind,
-                                                      lapply(fst_pairs, function(pop) getFst(file.path("results/piFst_outliers/mex2", r,
-                                                                                                       paste0(pop, ".fst.stats")), pop, region = r)))))
+FSTs <- do.call(rbind,
+                #lapply(regions_incl, function(r) do.call(rbind,
+                lapply(regions, function(r) do.call(rbind,
+                                                      lapply(fst_pairs, function(pop) 
+                                                        getFst(paste0("results/piFst_", name, "/mex2/", r,
+                                                                         "/", pop, ".fst.stats"), pop = pop, region = r)))))
+
 ggplot(FSTs, aes(x = pop, y = fst_Hudson, color = region)) +
   geom_point() + 
   geom_line(aes(group = region)) +
-  ggtitle("fst across groups for high mexicana ancestry ZAnc outlier regions")
-ggsave(filename = "fst_outliers.png", device = "png",
+  ggtitle(paste0("fst across groups for high mex ", name))
+ggsave(filename = paste0("plots/fst_groups_", name, ".png"), device = "png",
        plot = last_plot(), 
        width = 10, height = 10, units = "in",
        dpi = 300)
@@ -76,45 +90,97 @@ ggsave(filename = "fst_outliers.png", device = "png",
 
 
 thetasWind <- do.call(rbind,
-                  lapply(regions[c(1:3,6:26)], function(r) do.call(rbind,
-                                                      lapply(pops, function(pop) getTheta(file.path("results/piFst_outliers/mex2", r,
-                                                                                                    paste0(pop, ".thetasWindows.pestPG")), pop, region = r)))))
+                  lapply(regions, function(r) do.call(rbind, 
+                                                      lapply(groups, function(pop) 
+                                                        getTheta(paste0("results/piFst_", name, "/mex2/", r, "/", pop, ".thetasWindows.pestPG"), 
+                                                                 pop = pop, region = r)))))
+                        
 for (j in regions){
   p <- thetasWind %>%
     filter(region==j) %>%
     ggplot(., aes(x = pos, y = theta_pi, color = pop)) +
     geom_point() + 
-    ggtitle(paste0("pi diversity for high mexicana ancestry ZAnc outlier: ", j))
-  ggsave(filename = paste0("pi_region_", j, ".png"), device = "png",
+    ggtitle(paste0("pi for high mex ", name, ": ", j))
+  ggsave(filename = paste0("plots/pi_", name, "_region_", j, ".png"), 
+         device = "png",
          plot = p, 
          width = 10, height = 5, units = "in",
          dpi = 300)
 }
 
 
+thetas_ind <- do.call(rbind,
+                  lapply(regions, function(r) do.call(rbind,
+                                                      lapply(unique(pops$pop), function(pop) getTheta(paste0("results/piFst_", name, "/mex2/", r, "/", pop, ".thetasAll.pestPG"), 
+                                                                                          pop = pop, region = r)))))
+# compare pi across groups to pi across pops for outlier regions:
+left_join(pops, thetas_ind) %>%
+  ggplot(., aes(x = region, y = theta_pi, color = group)) +
+  geom_point() + 
+  geom_line(aes(group = pop)) +
+  ggtitle(paste0("pi across pops for high mex ", name))
+ggsave(filename = paste0("plots/pi_ind_pops_", name, ".png"), 
+       device = "png",
+       width = 10, height = 5, units = "in",
+       dpi = 300)
+left_join(pops, thetas_ind) %>%
+  filter(group == "symp.maize") %>%
+  ggplot(., aes(x = region, y = theta_pi, color = LOCALITY)) +
+  geom_point() + 
+  geom_line(aes(group = pop)) +
+  ggtitle(paste0("pi across symp. maize pops for high mex ", name))
+left_join(pops, thetas_ind) %>%
+  ggplot(., aes(x = region, y = theta_pi, color = LOCALITY)) +
+  geom_point(aes(group = pop)) + 
+  facet_wrap(~group) +
+  ggtitle(paste0("pi across locality pops for high mex ", name))
+ggsave(filename = paste0("plots/pi_locality_pops_", name, ".png"), 
+       device = "png",
+       width = 10, height = 5, units = "in",
+       dpi = 300)
+left_join(pops, thetas_ind) %>%
+  group_by(group, region) %>%
+  summarize(theta_pi = mean(theta_pi)) %>%
+  ggplot(., aes(x = region, y = theta_pi, color = group)) +
+  geom_point() + 
+  geom_line(aes(group = group)) +
+  ggtitle(paste0("pi mean across pops for high mex ", name))
+ggsave(filename = paste0("plots/pi_mean_pops_", name, ".png"), 
+       device = "png",
+       width = 10, height = 5, units = "in",
+       dpi = 300)
+ggplot(thetas, aes(x = region, y = theta_pi, color = pop)) +
+  geom_point() + 
+  geom_line(aes(group = pop)) +
+  ggtitle(paste0("pi across groups of ind. for high mex ", name))
+ggsave(filename = paste0("plots/pi_grouped_inds_", name, ".png"), 
+       device = "png",
+       width = 10, height = 5, units = "in",
+       dpi = 300)
+#pops_incl[c(1:10, 12, 14:16, 18:22)]
+thetasWind_ind <- do.call(rbind,
+                      lapply(regions, function(r) do.call(rbind, 
+                                                          lapply(unique(pops$pop), function(pop) 
+                                                            getTheta(paste0("results/piFst_", name, "/mex2/", r, "/", pop, ".thetasWindows.pestPG"), 
+                                                                     pop = pop, region = r)))))
+
+#d <- read.table(paste0("results/piFst_", name, "/mex2/", regions[1], "/", unique(pops$pop)[5], ".thetasWindows.pestPG"), sep = "\t") 
 
 
-do.call(rbind,
-                lapply(regions, function(r) do.call(rbind,
-                                                    lapply(fst_pairs, function(pop) file.exists(file.path("results/piFst_outliers/mex2", r,
-                                                                                                     paste0(pop, ".fst.stats")))))))
-do.call(rbind,
-        lapply(regions, function(r) do.call(rbind,
-                                            lapply(fst_pairs, function(pop) with(file.info(file.path("results/piFst_outliers/mex2", r,
-                                                                                                  paste0(pop, ".fst.stats"))), size <18)))))
+for (j in unique(thetasWind_ind$region)){
+  p <- left_join(thetasWind_ind, pops, by = "pop") %>%
+    filter(region==j) %>%
+    ggplot(., aes(x = pos/1000, y = theta_pi, color = LOCALITY)) +
+    xlab("pos (kb)") +
+    geom_line() + 
+    ggtitle(paste0("pi for high mex ", name, ": ", j)) +
+    facet_wrap(~group)
+  plot(p)
+  ggsave(filename = paste0("plots/pi_ind_pop_", name, "_region_", j, ".png"), 
+         device = "png",
+         plot = p, 
+         width = 10, height = 5, units = "in",
+         dpi = 300)
+}
 
 
-lapply(fst_pairs, function(pop) getFst(file.path("results/piFst_outliers/mex2", "chr7_168608960-169087801",
-                 paste0(pop, ".fst.stats")), pop = pop, region ="chr7_168608960-169087801"))
-
-
-thetasWind <- do.call(rbind,
-                  lapply(1:nrow(outliers), function(r) do.call(rbind,
-                                                               lapply(pops, function(pop) getTheta(file.path(paste0("results/piFst_outliers/mex2/chr", outliers[i, "chr"], "_", outliers[i,"start"], "-", outliers[i, "end"]),
-                                                                                                             paste0(pop, ".thetasWindows.pestPG")), pop, region = r)))))
-
-
-
-thetasWind <- do.call(rbind,
-                  lapply(pops, function(pop) getTheta(file.path(dir, paste0(pop, ".thetasWindows.pestPG")), pop, region)))
-                 
