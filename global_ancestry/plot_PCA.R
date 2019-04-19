4aa3483d180045b037c5b1f0bc64dc57dbba2ef5
Erin Calfee <erincalfee@gmail.com>
2019-04-19 10:16:18 -0700

    plotting pca with duplicates

--- global_ancestry/plot_PCA.R
+++ global_ancestry/plot_PCA.R
@@ -13,8 +13,13 @@ colors_alphabetical = colors_maize2mex[c(1,4,2,3)] # allo maize, allo mex, symp 
 
 PREFIX <- "hilo_alloMAIZE_MAIZE4LOW_RIMMA0625_small"
 PREFIX <- "duplicates"
+PREFIX <- "hilo_alloMAIZE_MAIZE4LOW_RIMMA0625_small_and_duplicates"
+ids_dup = unlist(lapply(data.frame(ID = read.table(paste0("../samples/duplicates_IDs.list"), header = F, stringsAsFactors = F)$V1, stringsAsFactors = F),
+                        function(x) rep(x, 2)))
+ids_merged = read.table(paste0("../samples/", "hilo_alloMAIZE_MAIZE4LOW_RIMMA0625_small", "_IDs.list"), header = F, stringsAsFactors = F)$V1
+IDs = data.frame(ID = c(ids_merged, ids_dup))
 #PREFIX <- "missing268-276_hilo_alloMAIZE_MAIZE4LOW"
-IDs <- data.frame(ID = read.table(paste0("../samples/", PREFIX, "_IDs.list"), header = F, stringsAsFactors = F)$V1, stringsAsFactors = F)
+#IDs <- data.frame(ID = read.table(paste0("../samples/", PREFIX, "_IDs.list"), header = F, stringsAsFactors = F)$V1, stringsAsFactors = F)
 metrics <- read.table(paste0("../filtered_bams/metrics/", PREFIX, ".flagstat.total"), header = F, stringsAsFactors = F)
 colnames(metrics) <- c("ID", "total_reads_pass")
 hilo <- read.table("../samples/hilo_meta.txt", stringsAsFactors = F, header = T, sep = "\t")
@@ -560,6 +565,15 @@ d %>%
 # this is pointless - I need to include a broader range of samples in my PCA and then see what's happening with these.
 pca_no16 <- eigen(cov_data[IDs$ID != "HILO16", IDs$ID != "HILO16"])
 #identify(pca_no16$vectors[,1], pca_no16$vectors[,2])
+
+d %>%
+  ggplot(., aes(x = PC1, y = PC2, color = ID)) + 
+  geom_point(alpha = .5) + 
+  xlab(paste0("PC1 (", PC_var_explained[1], "%)")) +
+  ylab(paste0("PC2 (", PC_var_explained[2], "%)")) +
+  ggtitle("duplicates")
+
+
 
 # TO DO: possibly rerun PCAngsd with separate entries for bams of the same individual,
 # but before merging across sequencing pools to confirm it's the same sample
