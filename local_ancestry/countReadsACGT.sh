87d7d9c1ab94728e57adc4a976bcdce74ed0ba66
Erin Calfee <erincalfee@gmail.com>
2019-04-28 10:02:35 -0700

    updated ACGT and major/minor allele counts scripts

--- local_ancestry/countReadsACGT.sh
+++ local_ancestry/countReadsACGT.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 #SBATCH --partition=med
-#SBATCH -D /home/ecalfee/hilo/data
+#SBATCH -D /home/ecalfee/hilo/local_ancestry
 #SBATCH -J countACGT
 #SBATCH -o /home/ecalfee/hilo/slurm-log/countACGT_%A_%a.out
 #SBATCH -t 4:00:00
 #SBATCH --mem=8G
-#SBATCH --array=1-200
-#SBATCH --export="CHR=1,ALL"
+
+# to run: local_ancestry$ sbatch --array=0-217 --export="CHR=1,PREFIX=pass2_alloMAIZE,ALL" countReadsACGT.sh
 
 # general bash script settings to make sure if any errors in the pipeline fail
 # then it’s a ‘fail’ and it passes all errors to exit and allows no unset variables
@@ -18,42 +18,45 @@ set –o nounset
 # columns totA totC totG totT as well as a .pos.gz file with all positions with data
 # for a low-coverage hilo individual
 
-# array is the hilo ID #
-id=$SLURM_ARRAY_TASK_ID
-dir="geno_lik/merged_pass1_all_alloMaize4Low_16/thinnedHMM"
-bam_dir="filtered_bam"
-bam_path=${bam_dir}"/hilo_"${id}".sort.dedup.baq.bam"
-DIR_OUT=${dir}"/countsACGT/chr"${CHR}
-DIR_SCRATCH="/scratch/ecalfee/countACGT_hilo_"${id}"_chr"${i}
 MAX_DEPTH_IND=8
+REF="../data/refMaize/Zea_mays.B73_RefGen_v4.dna.toplevel.fa"
+i=$SLURM_ARRAY_TASK_ID # which hilo sample from list file
+ids=($(cat ../samples/"$PREFIX"_IDs.list))
+ID=${ids["$i"]} # sample ID
+bams=($(cat ../samples/"$PREFIX"_bams.list))
+BAM=${bams["$i"]} # path to individual bam
+
+DIR_SITES="results/thinnedSNPs/$PREFIX/"
+DIR_OUT="results/countsACGT/$PREFIX/$ID"
+DIR_SCRATCH="/scratch/ecalfee/countACGT_"$ID"_chr$i"
 
 # load modules
 # includes angsd v9.21
 module load bio
 
 # make output and scratch directories
-mkdir -p ${DIR_OUT}
-mkdir -p ${DIR_SCRATCH}
+mkdir -p "$DIR_OUT"
+mkdir -p "$DIR_SCRATCH"
 
 # make file with single focus bam listed
-mkdir -p ${bam_dir}/list_indiv_bams
-echo ${bam_path} > ${bam_dir}/list_indiv_bams/hilo_${id}.only.list
+mkdir -p "../samples/"$PREFIX"_list_individuals"
+echo "$BAM" > "../samples/"$PREFIX"_list_individuals/"$ID"_bams.list"
 
-echo "counting reads supporting ACGT alleles for chr"${CHR}" and individual HILO"${id}
+echo "counting reads supporting ACGT alleles for chr"${CHR}" and individual "${ID}
 # counts reads for sites in sites file that pass basic quality filtering
-angsd -out ${DIR_SCRATCH}/hilo_${id} \
--ref refMaize/AGPv4.fa \
--bam ${bam_dir}/list_indiv_bams/hilo_${id}.only.list \
+angsd -out "$DIR_SCRATCH/chr$CHR" \
+-ref "$REF" \
+-bam "../samples/"$PREFIX"_list_individuals/"$ID"_bams.list" \
 -minQ 20 -minMapQ 30 \
 -doCounts 1 -dumpCounts 3 \
--setMaxDepthInd ${MAX_DEPTH_IND} \
+-setMaxDepthInd "$MAX_DEPTH_IND" \
 -remove_bads 1 \
--r ${CHR} \
--sites ${dir}/chr${CHR}.var.sites
+-r "$CHR" \
+-sites "$DIR_SITES/chr$CHR.var.sites"
 
 # transfer output files to home directory
 echo "all done counting ACGT! Now transfering files from local to home directory"
-rsync -avh --remove-source-files ${DIR_SCRATCH}/ ${DIR_OUT}/ # copies all contents of output directory over to the appropriate home directory & cleans up scratch dir.
+rsync -avh --remove-source-files "$DIR_SCRATCH/" "$DIR_OUT/" # copies all contents of output directory over to the appropriate home directory & cleans up scratch dir.
 echo "results copied to home output directory: "${DIR_OUT}
 
 echo "all done!"
--- local_ancestry/countReadsMajorMinor.sh
+++ local_ancestry/countReadsMajorMinor.sh
@@ -1,16 +1,19 @@
 #!/bin/bash -l
 #SBATCH --partition=med
-#SBATCH -D /home/ecalfee/hilo/data
+#SBATCH -D /home/ecalfee/hilo/local_ancestry
 #SBATCH -J countMajMin
 #SBATCH -o /home/ecalfee/hilo/slurm-log/countReadsMajorMinor_%A_%a.out
 #SBATCH -t 2:00:00
 #SBATCH --mem=4G
-#SBATCH --array=1-200
 
-# array contains all the chromosome #
-ID=$SLURM_ARRAY_TASK_ID
-# directory for input and output
-dir="geno_lik/merged_pass1_all_alloMaize4Low_16/thinnedHMM"
+# to run: local_ancestry$ sbatch --array=0-217 --export=PREFIX=pass2_alloMAIZE countReadsMajorMinor.sh
+
+i=$SLURM_ARRAY_TASK_ID # which hilo sample from list file
+ids=($(cat ../samples/"$PREFIX"_IDs.list))
+ID=${ids["$i"]} # sample ID
+DIR_IN="results/countsACGT/$PREFIX/$ID"
+DIR_OUT="results/countsMajMin/$PREFIX/$ID"
+DIR_SITES="results/thinnedSNPs/$PREFIX/"
 
 # general bash script settings to make sure if any errors in the pipeline fail
 # then it’s a ‘fail’ and it passes all errors to exit and allows no unset variables
@@ -18,12 +21,15 @@ set –o pipefail
 set –o errexit
 set –o nounset
 
+# make output DIRECTORY
+mkdir -p "$DIR_OUT"
+
 # load software needed
 module load R
 
 # run R script for each chromosome
 for CHR in {1..10}
     do echo "converting ACGT read counts to major/minor read counts for hilo"$ID" chr"$CHR
-    Rscript ../scripts/count_reads_major_minor.R $CHR $ID $dir
+    Rscript ./count_reads_major_minor.R $CHR $ID $DIR_SITES $DIR_IN $DIR_OUT
   done
 echo 'all done!'
