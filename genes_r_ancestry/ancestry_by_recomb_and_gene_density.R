547830a9dd124519cc5dbd4c97c29783a473137a
ecalfee <erincalfee@gmail.com>
2019-05-16 11:57:56 -0700

    working on simple proof-of-concept test cases for ZAnc test

--- genes_r_ancestry/ancestry_by_recomb_and_gene_density.R
+++ genes_r_ancestry/ancestry_by_recomb_and_gene_density.R
@@ -924,6 +924,136 @@ zBeta_elev_maize <- apply(maize_anc, 1, function(l) # calculate slope for each l
 d$zBeta_elev_maize <- zBeta_elev_maize
 hist(d$zBeta_elev_maize)
 
+# returning more information about the model
+zBeta2_elev_maize <- apply(maize_anc, 1, function(l) # calculate slope for each locus
+  #zBeta(ancFreq = t(maize_anc[1,]),
+  zBeta2(ancFreq = l,
+        envWeights = meta.pops$ELEVATION[meta.pops$zea == "maize"], 
+        invL = zAnc_maize$InvL, 
+        alpha = zAnc_maize$alpha))
+zb2 <- data.frame(t(zBeta2_elev_maize)) # data frame is easier to work with
+with(zb2, plot(r_squared ~ slope.zEnv, col = ifelse(d$inv4m, "blue", "black")))
+with(zb2, plot(sum_sq_res ~ slope.zEnv, col = ifelse(d$inv4m, "blue", "black")))
+
+# not centering environment:
+zBeta3_elev_maize <- apply(maize_anc, 1, function(l) # calculate slope for each locus
+  zBeta3(ancFreq = l,
+         envWeights = meta.pops$ELEVATION[meta.pops$zea == "maize"], 
+         invL = zAnc_maize$InvL, 
+         alpha = zAnc_maize$alpha))
+zb3 <- data.frame(t(zBeta3_elev_maize)) # data frame is easier to work with
+with(zb3, plot(r_squared ~ slope.zEnv, col = ifelse(d$inv4m, "blue", "black"), main = "zb3"))
+with(zb3, plot(sum_sq_res ~ slope.zEnv, col = ifelse(d$inv4m, "blue", "black"), main = "zb3"))
+
+# what is the qualitative diff of centering? zb2 vs. zb3:
+plot(zb2$slope.zEnv ~ zb3$slope.zEnv) # eh blob
+cor(zb2$slope.zEnv, zb3$slope.zEnv) # very weak correlation, or = .04
+plot(zb2$pval_slope ~ zb3$pval_slope) # black box (everywhere)
+cor(zb2$pval_slope, zb3$pval_slope) # slightly negative, cor -.06
+plot(zb2$sum_sq_res ~ zb3$sum_sq_res) # highly correlated
+cor(zb2$sum_sq_res, zb3$sum_sq_res) # cor = .96
+
+
+# test set: there are 14 populations of maize. I will create some test sets with 14 pops
+# that have perfect environmental correlations
+# each row is a made-up ancestry frequency observation; each column is a population 
+test_case_names <- c("all_1", "last_pop_only", "sixth_pop_only", "first_pop_only", "linear_env")
+
+test_anc <- rbind(rep(1, 14), # all selected for mexicana ancestry
+                  c(zAnc_maize$alpha[1:13], 1), # only last population selected
+                  c(zAnc_maize$alpha[1:5], 1, zAnc_maize$alpha[7:14]), # 6th population selected
+                  c(1, zAnc_maize$alpha[2:14]), # last population selected
+                  seq(0, 1, length.out = 14)) # linear association with simple linear environmental gradient: e.g. seq(100, 200, length.out = 14)
+                  # could also test false positive cases: e.g. one outlier but no real other association with
+
+# I'll use my test_anc cases with the empirical K matrix:
+test_env <- rbind(rep(1, 14), # all selected for mexicana ancestry
+                   c(zAnc_maize$alpha[1:13], 1), # only last population selected
+                   c(zAnc_maize$alpha[1:5], 1, zAnc_maize$alpha[7:14]), # 6th population selected
+                   c(1, zAnc_maize$alpha[2:14]), # last population selected
+                   seq(0, 1, length.out = 14))
+
+# transformed test environments:
+apply(test_env, 1, function(e) zAnc_maize$InvL %*% e)
+apply(test_env, 1, function(e) zAnc_maize$InvL %*% (e - mean(e)))
+
+zTest2 <- lapply(1:nrow(test_env), function(e)
+  apply(test_anc, 1, function(l) # calculate slope for each locus
+  #zBeta(ancFreq = t(maize_anc[1,]),
+  zBeta2(ancFreq = l,
+         envWeights = test_env[e, ], 
+         invL = zAnc_maize$InvL, 
+         alpha = zAnc_maize$alpha)))
+
+zTest3 <- lapply(1:nrow(test_env), function(e)
+  apply(test_anc, 1, function(l) # calculate slope for each locus
+    #zBeta(ancFreq = t(maize_anc[1,]),
+    zBeta3(ancFreq = l,
+           envWeights = test_env[e, ], 
+           invL = zAnc_maize$InvL, 
+           alpha = zAnc_maize$alpha)))
+a2 <- do.call(rbind,
+             lapply(1:length(zTest2), function(l)
+  data.frame(t(zTest2[[l]])) %>%
+  mutate(test_model = test_case_names) %>%
+  mutate(true_model = test_case_names[l])))
+a2 %>%
+  ggplot(aes(x = test_model, y = r_squared, color = true_model, shape = (true_model == test_model))) +
+  geom_point() + 
+  ggtitle("mean centered environment test cases")
+a2 %>%
+  gather(., "model_output", "value", c("intercept..Intercept.", "slope.zEnv", "r_squared")) %>%
+  ggplot(aes(x = test_model, y = value, color = true_model, shape = (true_model == test_model))) +
+  geom_point() + 
+  ggtitle("mean centered environment test cases") +
+  facet_wrap(~model_output) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave("plots/test_cases_environment_ZAnc_model_values_centered_env.png",
+       height = 10, width = 10, device = "png", units = "in")
+
+a2 %>%
+  gather(., "model_output", "value", c("sum_sq_res", "pval_intercept", "pval_slope.zEnv")) %>%
+  mutate(., log_value = log(value)) %>%
+  ggplot(aes(x = test_model, y = log_value, color = true_model, shape = (true_model == test_model))) +
+  geom_point() + 
+  ggtitle("mean centered environment test cases") +
+  facet_wrap(~model_output) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave("plots/test_cases_environment_ZAnc_fit_values_centered_env.png",
+       height = 10, width = 10, device = "png", units = "in")
+
+
+a3 <- do.call(rbind,
+              lapply(1:length(zTest3), function(l)
+                data.frame(t(zTest3[[l]])) %>%
+                  mutate(test_model = test_case_names) %>%
+                  mutate(true_model = test_case_names[l])))
+a3 %>%
+  ggplot(aes(x = test_model, y = intercept..Intercept., color = true_model, shape = (true_model == test_model))) +
+  geom_point() + 
+  ggtitle("not mean centered environment test cases")
+a3 %>%
+  gather(., "model_output", "value", c("intercept..Intercept.", "slope.zEnv", "r_squared")) %>%
+  ggplot(aes(x = test_model, y = value, color = true_model, shape = (true_model == test_model))) +
+  geom_point() + 
+  ggtitle("not mean centered environment test cases") +
+  facet_wrap(~model_output) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave("plots/test_cases_environment_ZAnc_model_values.png",
+       height = 10, width = 10, device = "png", units = "in")
+
+a3 %>%
+  gather(., "model_output", "value", c("sum_sq_res", "pval_intercept", "pval_slope.zEnv")) %>%
+  mutate(., log_value = log(value)) %>%
+  ggplot(aes(x = test_model, y = log_value, color = true_model, shape = (true_model == test_model))) +
+  geom_point() + 
+  ggtitle("not mean centered environment test cases") +
+  facet_wrap(~model_output) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave("plots/test_cases_environment_ZAnc_fit_values.png",
+       height = 10, width = 10, device = "png", units = "in")
+
+
 # straight regression, no rotation:
 ancBeta_elev_maize <- apply(maize_anc, 1, function(l) # calculate slope for each locus
   ancBeta(ancFreq = l,
