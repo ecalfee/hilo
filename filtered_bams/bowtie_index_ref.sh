8ed4f05371fc1b2f5ca3340fd84b87a8875f4b9e
Erin Calfee <erincalfee@gmail.com>
2020-04-20 22:20:59 -0700

    script to index ref with bowtie to run fastq_screen check on new fastq files

---
+++ filtered_bams/bowtie_index_ref.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --partition=med2
+#SBATCH -D /home/ecalfee/hilo/data/refMaize
+#SBATCH -J flagStat
+#SBATCH -o /home/ecalfee/hilo/slurm-log/bowtieIndexRef_%A_%a.out
+#SBATCH -t 6:00:00
+#SBATCH --mem=24G
+
+# to run: sbatch bowtieIndexRef.sh
+
+# script creates bowtie2 index for reference genome (to use with fastq_screen)
+# general bash script settings to make sure if any errors in the pipeline fail
+# then it’s a ‘fail’ and it passes all errors to exit and allows no unset variables
+set –o pipefail
+set –o errexit
+set –o nounset
+
+# load modules
+module load bio3
+module load bowtie2
+
+bowtie2-build -f Zea_mays.B73_RefGen_v4.dna.toplevel.fa Zea_mays.B73_RefGen_v4.dna.toplevel.fa
+
+echo "all done!"
