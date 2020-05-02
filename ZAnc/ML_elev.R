1dbd96e3d5148a8ea581044dfa68a01e055ae90d
Erin Calfee <erincalfee@gmail.com>
2020-05-01 22:15:00 -0700

    implemented maximum likelihood approach for estimating slope with environment

--- ZAnc/ML_elev.R
+++ ZAnc/ML_elev.R
@@ -9,6 +9,9 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
+# functions
+source("k_matrix.R")
+
 # load data 
 PREFIX="pass2_alloMAIZE"
 
@@ -23,30 +26,6 @@ meta.pops <- read.table(paste0("../global_ancestry/results/NGSAdmix/", PREFIX, "
   left_join(., pop_elev, by = c("popN")) %>%
   left_join(., unique(dplyr::select(hilo, c("popN", "zea", "symp_allo", "RI_ACCESSION", "GEOCTY", "LOCALITY"))), by = c("popN")) %>%
   mutate(pop = paste0("pop", popN))
-
-# genomic sites information
-# SNPs with ancestry calls
-dir_sites = paste0("../local_ancestry/results/thinnedSNPs/", PREFIX)
-sites <- do.call(rbind, 
-                 lapply(1:10, function(i)
-                   read.table(paste0(dir_sites, "/chr", i, ".var.sites"),
-                              header = F, stringsAsFactors = F)))
-colnames(sites) <- c("chr", "pos", "major", "minor")
-# get inversions:
-inv = read.table("../data/refMaize/inversions/knownInv_v4_coord.txt",
-                 stringsAsFactors = F, header = T)
-colnames(inv) = c("ID", "chr", "start", "end", "length")
-excl_inv_buffer = 1000000 # exclude 1 megabase around inversion
-inv$excl_start = inv$start - excl_inv_buffer
-inv$excl_end = inv$end + excl_inv_buffer
-# which sites are within the inversion?
-sites <- mutate(sites, inv4m_1Mb_buffer = (chr == inv$chr[inv$ID=="inv4m"] & 
-                                             pos >= inv$excl_start[inv$ID=="inv4m"] & 
-                                             pos <= inv$excl_end[inv$ID == "inv4m"]),
-                inv4m = (chr == inv$chr[inv$ID=="inv4m"] & 
-                           pos >= inv$start[inv$ID=="inv4m"] & 
-                           pos <= inv$end[inv$ID == "inv4m"])) 
-
 
 # ancestry calls
 LOCAL_ANC_SUBDIR="output_noBoot"
@@ -83,24 +62,29 @@ X = cbind(intercept = 1, elev_c) %>%
   as.matrix(.)
 invK = solve(K)
 
-# generalized least squares model for population ancestries centered at pop mean ancestry:
-# (y - alpha)
-# take out part1 of the generalized least squares model to not repeat calcs:
-part1 = solve(t(X) %*% invK %*% X) %*% t(X) %*% invK
-# now apply to each observed vector of population ancestries
+# estimate beta for each observed vector of population ancestries
 betas = t(apply(maize_anc, 1, function(y) 
-  part1 %*% (y - alpha))) %>%
+  ML_b(y = y, alpha = alpha, invK = invK, X = X)))
+
+# estimate log likelihood under this MVN model
+detK = det(K) # determinant of K matrix
+logliks <- sapply(1:nrow(maize_anc), function(i)
+                  ll_mvn(maize_anc[i, ], 
+                         mu = alpha + X %*% t(betas[i, ]), # use ML beta estimate calculate expected value
+                         detK = detK, 
+                         invK = invK))
+
+fits <- betas %>% # put everything together
   as.data.frame(.) %>%
-  data.table::setnames(c("b0", "bElev"))
+  data.table::setnames(c("b0", "b1")) %>%
+  mutate(ll = logliks,
+         k = 2,
+         n = length(alpha),
+         AICc = AICc(ll = ll, k = k, n = n))
 
 write.table(betas,
-            paste0("results/models/", PREFIX, "/maize/gls_elevation.txt"),
+            paste0("results/models/", PREFIX, "/maize/ML_elevation.txt"),
             sep = "\t",
             quote = F,
             col.names = T, 
             row.names = F)
-
-#zAnc3_mex <- t(apply(mexicana_anc, 1, function(l) zAnc(ancFreq = l, 
-#                                                       invL = zAnc_mexicana$InvL, 
-#                                                       alpha = zAnc_mexicana$alpha)))
-#colnames(zAnc3_mex) <- colnames(mexicana_anc)
--- ZAnc/zEnv_sims.R
+++ ZAnc/zEnv_sims.R
@@ -1,18 +1,29 @@
 # simple simulations of environmental associations + MVN noise
 library(dplyr)
 library(tidyr)
+library(broom)
 library(ggplot2)
 source("k_matrix.R")
 
 # functions
-# truncate to [0,1] range
-truncate01 <- function(x){
-  t <- x
-  t[t < 0] <- 0
-  t[t > 1] <- 1
-  return(t)
-}
-
+# function to minimize neg. loglik and return beta's and loglik
+# for environmental selection model
+fit_env <- function(y, X, alpha, detK, invK, params){
+  o = optim(par = c(0,0),
+            fn = function(b) -ll_mvn(y = y,
+                                     mu = X %*% b + alpha,
+                                     detK = detK,
+                                     invK = invK))
+  b = broom::tidy(o) %>%
+    pivot_wider(data = ., 
+                names_from = parameter,
+                values_from = value) %>%
+    data.table::setnames(params)
+  fit = bind_cols(b, broom::glance(o)) %>%
+    mutate(ll = -value) %>%
+    dplyr::select(-value)
+  return(fit)
+  }
 
 # read in data (or could pick arbitrary alpha's and K matrices)
 # load data 
@@ -37,8 +48,9 @@ K <- read.table("results/zAnc_K_matrix_maize.txt", sep = "\t", header = T) %>%
   as.matrix(.)
 alpha <- read.table("results/zAnc_alpha_maize.txt", sep = "\t", header = T)$x
 names(alpha) <- colnames(K)
-n_sim = 1000
-set.seed(101)
+#n_sim = 10^5
+n_sim = 10^3 # just quick test
+set.seed(500)
 # mvn (neutral)
 mvn <- mvrnorm(n = n_sim, 
                          mu = alpha, 
@@ -137,7 +149,11 @@ sel_betas_pretty %>% # top row is the neutral model
                                bElev = slopes) %>%
                pivot_longer(., c("b0", "bElev"), names_to = "b", values_to = "estimate"),
                aes(xintercept = estimate)) +
-  facet_grid(name ~ b, scales = "free_x")
+  facet_grid(name ~ b, scales = "free_x") +
+  theme(legend.position = "none") +
+  ggtitle("anc ~ b0 + bElev*elevation - no truncation")
+ggsave("plots/sims_gls_b0_bElev_no_trunc.png",
+       height = 10, width = 7, units = "in", dpi = 300)
 
 # power to detect a selected locus at 5% FDR:
 #table(mvn == sel[[1]]) # all True
@@ -197,7 +213,102 @@ sel_trunc_betas_pretty %>% # top row is the neutral model
                                bElev = slopes) %>%
                pivot_longer(., c("b0", "bElev"), names_to = "b", values_to = "estimate"),
              aes(xintercept = estimate)) +
-  facet_grid(name ~ b, scales = "free_x")
+  facet_grid(name ~ b, scales = "free_x") +
+  theme(legend.position = "none") +
+  ggtitle("anc ~ b0 + bElev*elevation - [0,1]")
+ggsave("plots/sims_gls_b0_bElev_01_trunc.png",
+       height = 10, width = 7, units = "in", dpi = 300)
 
+# calculate FDRs:
+
+# TO DO: each model should output also SSE for each locus to compare
+# false-discovery rate for top gls bElev outliers:
+# load gls b0 and bElev estimates:
+gls_bElev <- read.table("results/models/pass2_alloMAIZE/maize/gls_elevation.txt",
+                        header = T, sep = "\t")
+head(gls_bElev)
+v_bElev <- seq(from = min(gls_bElev$bElev), 
+               to = max(gls_bElev$bElev),
+               by = .001)
+over_null = sapply(v_bElev, function(x) sum(mvn_trunc_betas[ , 2] > x))/nrow(mvn_trunc_betas)
+over_data = sapply(v_bElev, function(x) sum(gls_bElev$bElev > x))/length(gls_bElev$bElev)
+fdrs = over_null/over_data # expected false-positives rate divided by total positive rate
+plot(1:length(fdrs), fdrs, type = "l")
+abline(h = 0.05, col = "blue")
+# which slope value is 5% FDR at? And what % of genome is above it? Look across chromosomes with that cutoff
+fdr5 <- v_bElev[which(fdrs < .05)][1]
+fdr1 <- v_bElev[which(fdrs < .01)][1]
+mean(gls_bElev$bElev > fdr5)
+mean(gls_bElev$bElev > fdr1)
+
+# genomic sites information
+# SNPs with ancestry calls
+dir_sites = paste0("../local_ancestry/results/thinnedSNPs/", PREFIX)
+sites <- do.call(rbind, 
+                 lapply(1:10, function(i)
+                   read.table(paste0(dir_sites, "/chr", i, ".var.sites"),
+                              header = F, stringsAsFactors = F)))
+colnames(sites) <- c("chr", "pos", "major", "minor")
+# get inversions:
+inv = read.table("../data/refMaize/inversions/knownInv_v4_coord.txt",
+                 stringsAsFactors = F, header = T)
+colnames(inv) = c("ID", "chr", "start", "end", "length")
+excl_inv_buffer = 1000000 # exclude 1 megabase around inversion
+inv$excl_start = inv$start - excl_inv_buffer
+inv$excl_end = inv$end + excl_inv_buffer
+# which sites are within the inversion?
+sites <- mutate(sites, inv4m_1Mb_buffer = (chr == inv$chr[inv$ID=="inv4m"] & 
+                                             pos >= inv$excl_start[inv$ID=="inv4m"] & 
+                                             pos <= inv$excl_end[inv$ID == "inv4m"]),
+                inv4m = (chr == inv$chr[inv$ID=="inv4m"] & 
+                           pos >= inv$start[inv$ID=="inv4m"] & 
+                           pos <= inv$end[inv$ID == "inv4m"])) 
+r <- cbind(sites, gls_bElev) %>%
+  mutate(FDR = ifelse(bElev > fdr1, "0.01", 
+                      ifelse(bElev > fdr5,
+                      "0.05", "n.s.")))
+#r[c(T, rep(F, 99)), ] %>%
+r %>%
+  ggplot(., aes(x = pos, y = bElev, color = FDR)) +
+  geom_point(size = .01) +
+  facet_wrap(~chr)
+
+# finding log likelihood of models
+# sanity check: confirming that optim and analytical solutions are the same:
+detK = det(K)
+# null model
+ll_mvn(y = sel[[1]][1,], 
+       mu = alpha, 
+       detK = detK, 
+       invK = invK)
+# selection model
+ll_mvn(y = sel[[1]][1,], 
+       mu = X %*% sel_betas[[1]][1,] + alpha, 
+       detK = detK, 
+       invK = invK)
+sel_optims = lapply(sel, function(s) 
+  do.call(rbind, lapply(1:nrow(s), function(i)
+    fit_env(y = s[i,], X = X, alpha = alpha, detK = detK, 
+            invK = invK, params = c("b0", "b1")))))
+sel_trunc_optims = lapply(sel_trunc, function(s) 
+  do.call(rbind, lapply(1:nrow(s), function(i)
+    fit_env(y = s[i,], X = X, alpha = alpha, detK = detK, 
+            invK = invK, params = c("b0", "b1")))))
+
+# good, very close agreement between optim and analytical optimization to find beta
+plot(sel_optims[[1]]$b0 ~ sel_betas[[1]][ ,1])
+plot(sel_trunc_optims[[1]]$b1 ~ sel_trunc_betas[[1]][ ,2])
+sapply(1:length(sel_optims), function(i)
+  sum(abs(sel_optims[[i]]$b0 - sel_betas[[i]][ ,1]) < .005)
+)
+sapply(1:length(sel_optims), function(i)
+  sum(abs(sel_optims[[i]]$b1 - sel_betas[[i]][ , 2]) < .005)
+)
+sapply(1:length(sel_trunc_optims), function(i) # same with truncation
+  sum(abs(sel_trunc_optims[[i]]$b0 - sel_trunc_betas[[i]][ ,1]) < .005)
+)
+sapply(1:length(sel_trunc_optims), function(i)
+  sum(abs(sel_trunc_optims[[i]]$b1 - sel_trunc_betas[[i]][ ,2]) < .005)
+)
 
 
