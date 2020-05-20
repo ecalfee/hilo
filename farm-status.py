349db80ccc96b2a07e533aabc66609638bf8cb4f
Erin Calfee <erincalfee@gmail.com>
2020-05-20 13:12:55 -0700

    attempt to parse farm message

--- farm-status.py
+++ farm-status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import subprocess
 import sys
+import re
 
-jobid = sys.argv[1]
+#jobid = sys.argv[1]
+farm_message = re.search("Submitted batch job (\d+)", sys.argv[1])
+jobid = farm_message.group(1)
 
 output = str(subprocess.check_output("sacct -j %s --format State --noheader | head -1 | awk '{print $1}'" % jobid, shell=True).strip())
 
