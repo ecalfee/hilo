a6683773f345fa73f78aa772f66080860ce5e5fe
Erin Calfee <erincalfee@gmail.com>
2020-05-19 18:13:42 -0700

    basic mapping and read filtering pipeline works on test data for snakemake

--- submit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#module load jdk/1.7.0_79
-
-snakemake --jobs 50 \
-    --rerun-incomplete \
-    --latency-wait 60 \
-    --cluster-config submit.json  \
-    --cluster "sbatch -p {cluster.p} --mem {cluster.mem} --cpus-per-task {cluster.cpus-per-task} --time {cluster.time} --job-name {cluster.name} -e {cluster.e} -o {cluster.o}"
