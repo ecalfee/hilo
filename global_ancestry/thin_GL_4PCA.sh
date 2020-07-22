22fbecaae9b9eff6ded978252bf542e3b0abe761
Erin Calfee <erincalfee@gmail.com>
2020-07-22 00:38:41 -0700

    running thin GL every 100th as it's own script _4PCA

--- global_ancestry/thin_GL_4PCA.sh
+++ global_ancestry/thin_GL_4PCA.sh
@@ -9,7 +9,7 @@ startR=$2
 endR=$3
 PREFIX=$4
 DIR_GL="variant_sites/results/$PREFIX"
-DIR_OUT="global_ancestry/results/thinnedSNPs/$PREFIX/thinnedSNPs"
+DIR_OUT="global_ancestry/results/thinnedSNPs/$PREFIX"
 
 
 # general bash script settings to make sure if any errors in the pipeline fail
@@ -20,8 +20,11 @@ set –o nounset
 
 # make output DIRECTORY
 #mkdir -p $DIR_OUT
+echo "working directory: "$PWD
+echo "input: "$DIR_GL"/region_"$startR".beagle.gz"
+echo "output:  "$DIR_OUT"/whole_genome.beagle.gz"
 # concatenate header from first file with every Nth SNP from each subsequent file
 zcat $DIR_GL/region_$startR.beagle.gz | head -n 1 | gzip > $DIR_OUT/whole_genome.beagle.gz; \
 for (( i=$startR; i<=$endR; i++ )); do zcat $DIR_GL/region_$i.beagle.gz | tail -n +2; done | awk -v N=$N 'NR % N == 0' | gzip >> $DIR_OUT/whole_genome.beagle.gz
 
-echo "done running with cat beagle.gz every $N th position, regions $startN to $endN, sample set: $PREFIX"
+echo "done running with cat beagle.gz every $N th position, regions $startR to $endR, sample set: $PREFIX"
