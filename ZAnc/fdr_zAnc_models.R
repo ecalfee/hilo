ade3460014207b44b8428973fbc67a6bf1d9bfb6
Erin Calfee <erincalfee@gmail.com>
2020-10-13 13:40:39 -0700

    narrowed range I need to test for FDR from 90th to just over 100th percentile for simulated MVN

--- ZAnc/fdr_zAnc_models.R
+++ ZAnc/fdr_zAnc_models.R
@@ -21,38 +21,38 @@ load(fit_in)
 # calculate false discovery rates
 # use neutral MVN simulations, not theoretical chisq(df = 1) 
 # to set significance at 5% FDR
-
+increment = 0.0001 # test for FDR at values incrementing by this amount
 # range of values to test go from top 10% of null simulations
 # (all of test data would have to be above that threshold to meet 10% FDR)
-# to maximum value in test data
+# to just one increment over the maximum value in simulated data (above this max there are no predicted false positives)
 print("ranges tested for FDR:")
 print("zElev")
 print(quantile(fits_sim$diff_elev_ll2, 1 - max(FDR_range)))
-print(max(fits$diff_elev_ll2))
+print(max(fits_sim$diff_elev_ll2) + increment)
 print("zAll")
 print(quantile(fits_sim$diff_all_ll2, 1 - max(FDR_range)))
-print(max(fits$diff_all_ll2))
+print(max(fits_sim$diff_all_ll2) + increment)
 print("zTz")
 print(quantile(fits_sim$zTz, 1 - max(FDR_range)))
-print(max(fits$zTz))
+print(max(fits_sim$zTz) + increment)
 
 # calculate false-discovery rate -- unusually high values indicate statistical significance
 FDRs <- bind_rows(calc_FDR_high(d = fits$diff_elev_ll2, s = fits_sim$diff_elev_ll2, 
                                 FDR_values = FDR_range, 
                                 test_values = seq(quantile(fits_sim$diff_elev_ll2, 1 - max(FDR_range)), 
-                                                  max(fits$diff_elev_ll2), 
-                                                  by = .0001)) %>%
+                                                  max(fits_sim$diff_elev_ll2) + increment, 
+                                                  by = increment)) %>%
                     dplyr::mutate(model = "elev"),
                   calc_FDR_high(d = fits$diff_all_ll2, s = fits_sim$diff_all_ll2, 
                                 FDR_values = FDR_range, 
                                 test_values = seq(quantile(fits_sim$diff_all_ll2, 1 - max(FDR_range)), 
-                                                  max(fits$diff_all_ll2), 
+                                                  max(fits_sim$diff_all_ll2) + increment, 
                                                   by = .0001)) %>%
                     dplyr::mutate(model = "all"),
                   calc_FDR_high(d = fits$zTz, s = fits_sim$zTz, 
                                 FDR_values = FDR_range, 
                                 test_values = seq(quantile(fits_sim$zTz, 1 - max(FDR_range)), 
-                                                  max(fits$zTz), 
+                                                  max(fits_sim$zTz) + increment, 
                                                   by = .0001)) %>%
                     dplyr::mutate(model = "zTz"))
 
