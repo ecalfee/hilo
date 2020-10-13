066079c82518f9190ad85f04228fe66abbbb795c
Erin Calfee <erincalfee@gmail.com>
2020-10-13 08:53:59 -0700

    broke FDR into two components high and low that can be run in isolation. made FDR calcs for zAnc more efficient to run faster

--- ZAnc/FDR.R
+++ ZAnc/FDR.R
@@ -12,30 +12,48 @@ fdr_low <- function(a, data, sims){# low FDR for value a
 
 # Calculate 1% 5% and 10% FDRs
 FDR_range = c(0.1, 0.05, 0.01)
-calc_FDR <- function(d, s, FDR_values = FDR_range, 
-                     test_values = seq(0, 1, by = .0001)){
-  # calc (low) FDR for every value in test_values
-  test_fdr_low <- sapply(test_values, function(x) 
-    fdr_low(x, data = d, sims = s))
-  # find which values match the FDR thresholds in FDR_values
-  FDRs_low <- data.frame(thesholds = sapply(FDR_values, 
-                                            function(p) 
-                                              max(test_values[test_fdr_low < p], 
-                                                  na.rm = T)),
-                         FDR = FDR_values,
-                         tail = "low",
-                         stringsAsFactors = F)
+
+# function to calculate high tail FDR
+calc_FDR_high <- function(d, s, FDR_values, test_values){
   # calc (high) FDR for every value in test_values
   test_fdr_high <- sapply(test_values, function(x) 
     fdr_high(x, data = d, sims = s))
   # find which values match the FDR thresholds in FDR_values
   FDRs_high <- data.frame(thesholds = sapply(FDR_values, 
                                              function(p) 
-                                               min(test_values[test_fdr_high < p], 
+                                               min(test_values[test_fdr_high <= p], 
                                                    na.rm = T)),
                           FDR = FDR_values,
                           tail = "high",
                           stringsAsFactors = F)
+  return(FDRs_high)
+}
+
+# function to calculate low tail FDR
+calc_FDR_low <- function(d, s, FDR_values, test_values){
+  # calc (low) FDR for every value in test_values
+  test_fdr_low <- sapply(test_values, function(x) 
+    fdr_low(x, data = d, sims = s))
+  # find which values match the FDR thresholds in FDR_values
+  FDRs_low <- data.frame(thesholds = sapply(FDR_values, 
+                                            function(p) 
+                                              max(test_values[test_fdr_low <= p], 
+                                                  na.rm = T)),
+                         FDR = FDR_values,
+                         tail = "low",
+                         stringsAsFactors = F)
+  return(FDRs_low)
+}
+
+# putting it together - get both high and low FDRs
+calc_FDR <- function(d, s, FDR_values = FDR_range, 
+                     test_values = seq(0, 1, by = .0001)){
+  FDRs_low <- calc_FDR_high(d = d, s = s, 
+                           FDR_values = FDR_values,
+                           test_values = test_values)
+  FDRs_high <- calc_FDR_low(d = d, s = s, 
+                            FDR_values = FDR_values,
+                            test_values = test_values)
   FDRs <- rbind(FDRs_high, FDRs_low)
   return(FDRs)
 }
--- ZAnc/fit_zAnc_models.R
+++ ZAnc/fit_zAnc_models.R
@@ -68,25 +68,40 @@ fits_sim <- fit_zAnc(anc = data.frame(MVN_sim), alpha = K$alpha, invK = invK, de
 # calculate false discovery rates
 # use neutral MVN simulations, not theoretical chisq(df = 1) 
 # to set significance at 5% FDR
-FDRs <- bind_rows(calc_FDR(d = fits$diff_elev_ll2, s = fits_sim$diff_elev_ll2, 
+
+# range of values to test go from top 10% of null simulations
+# (all of test data would have to be above that threshold to meet 10% FDR)
+# to maximum value in test data
+print("ranges tested for FDR:")
+print("zElev")
+print(quantile(fits_sim$diff_elev_ll2, 1 - max(FDR_range)))
+print(max(fits$diff_elev_ll2))
+print("zAll")
+print(quantile(fits_sim$diff_all_ll2, 1 - max(FDR_range)))
+print(max(fits$diff_all_ll2))
+print("zTz")
+print(quantile(fits_sim$zTz, 1 - max(FDR_range)))
+print(max(fits$zTz))
+
+# calculate false-discovery rate -- unusually high values indicate statistical significance
+FDRs <- bind_rows(calc_FDR_high(d = fits$diff_elev_ll2, s = fits_sim$diff_elev_ll2, 
                  FDR_values = FDR_range, 
-                             test_values = seq(min(fits$diff_elev_ll2), 
+                             test_values = seq(quantile(fits_sim$diff_elev_ll2, 1 - max(FDR_range)), 
                                                max(fits$diff_elev_ll2), 
                                                by = .0001)) %>%
                   dplyr::mutate(model = "elev"),
-                calc_FDR(d = fits$diff_all_ll2, s = fits_sim$diff_all_ll2, 
+                calc_FDR_high(d = fits$diff_all_ll2, s = fits_sim$diff_all_ll2, 
                          FDR_values = FDR_range, 
-                         test_values = seq(min(fits$diff_all_ll2), 
+                         test_values = seq(quantile(fits_sim$diff_all_ll2, 1 - max(FDR_range)), 
                                            max(fits$diff_all_ll2), 
                                            by = .0001)) %>%
                   dplyr::mutate(model = "all"),
-                calc_FDR(d = fits$zTz, s = fits_sim$zTz, 
+                calc_FDR_high(d = fits$zTz, s = fits_sim$zTz, 
                          FDR_values = FDR_range, 
-                         test_values = seq(min(fits$zTz), 
+                         test_values = seq(quantile(fits_sim$zTz, 1 - max(FDR_range)), 
                                            max(fits$zTz), 
                                            by = .0001)) %>%
-                  dplyr::mutate(model = "zTz")) %>%
-  dplyr::filter(., tail == "high")
+                  dplyr::mutate(model = "zTz"))
 
 # what % of SNPs exceed these thresholds?
 FDRs$n_SNPs = sapply(1:nrow(FDRs), function(i) 
