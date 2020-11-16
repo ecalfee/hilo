05780eaeaa80cc2145c3f44bb58ba1bf824d1821
Erin Calfee <erincalfee@gmail.com>
2020-11-16 12:13:18 -0800

    added rule for plotting f4 results

--- ancestry_by_r/calc_and_plot_f4.R
+++ ancestry_by_r/calc_and_plot_f4.R
@@ -9,12 +9,12 @@ library(boot)
 # with no tripsacum reads mapped. See last line of depthSample -- hilo$ tail -n 1 ancestry_by_r/results/f4/sympatric_maize/W384.depthSample
 
 # load variables from Snakefile
-zea = snakemake@params[["zea"]]
-# zea = "maize"
-# zea = "mexicana"
 sympatric_pop = snakemake@params[["sympatric_pop"]]
 # sympatric_pop = "sympatric_maize"
 # sympatric_pop = "sympatric_mexicana"
+# zea is maize or mexicana. Only used for colors on plots. maize pops have index numbers > 100
+zea = ifelse(sympatric_pop == "sympatric_maize" || as.integer(substr(sympatric_pop, 4, 7)) > 100, "maize", "mexicana") 
+
 f4_num_file = snakemake@input[["f4_num"]]
 # f4_num_file = paste0("ancestry_by_r/results/f4/", sympatric_pop, ".f4")
 f4_denom_file = snakemake@input[["f4_denom"]]
@@ -35,7 +35,7 @@ png_f4_num_denom = snakemake@output[["png_f4_num_denom"]]
 # png_f4_num_denom = paste0("ancestry_by_r/plots/f4_", sympatric_pop, "_num_denom.png")
 n_boot = snakemake@params[["n_boot"]]
 # n_boot = 1000
-inv_file = snakemake@params[["inv"]]
+inv_file = snakemake@params[["inv_file"]]
 # inv_file = "data/refMaize/inversions/knownInv_v4_coord.txt"
 
 source(colors_file)
@@ -84,7 +84,9 @@ convert_2_Mb_per_cM <- function(bin){
   range = round(1/middle, 4) # 1cM*1/(cM/Mb) = Mb/cM, rounded to 4 decimal places
   return(paste0(start, range[2], ",", range[1], end))
 }
-convert_2_Mb_per_cM("(2,5.8]")
+# test:
+# convert_2_Mb_per_cM("(2,5.8]")
+
 # combine data
 d_f4 <- group_by(f4_num, window) %>%
   summarise(num_stat = sum(Numer),
@@ -127,7 +129,7 @@ f4_by_cd <- d_f4 %>%
 #View(f4_by_cd)
 #View(f4_by_r)
 
-with(f4_by_r, cor(x = quintile_Mbp5, y = f4_mex, method = "spearman"))
+# with(f4_by_r, cor(x = quintile_Mbp5, y = f4_mex, method = "spearman"))
 
 # bootstrap
 # function to calc stat
@@ -156,12 +158,12 @@ calc_cor_r <- function(d, i) { # d is data, i is indices
 }
 
 # test:
-filter(d_f4, !is.na(num_sites)) %>%
-  calc_cor_r(., 1:nrow(.)) # all windows
-filter(d_f4, !is.na(num_sites) & !inv4m) %>%
-  calc_cor_r(., 1:nrow(.)) # all windows not overlapping inversion
-filter(d_f4, !is.na(num_sites)) %>%
-  calc_cor_r(., sample(1:nrow(.), replace = T))
+#filter(d_f4, !is.na(num_sites)) %>%
+#  calc_cor_r(., 1:nrow(.)) # all windows
+#filter(d_f4, !is.na(num_sites) & !inv4m) %>%
+#  calc_cor_r(., 1:nrow(.)) # all windows not overlapping inversion
+#filter(d_f4, !is.na(num_sites)) %>%
+#  calc_cor_r(., sample(1:nrow(.), replace = T))
 
 # bootstrap across all windows
 boot_r <- boot(data = filter(d_f4, !is.na(num_sites)), 
@@ -179,16 +181,16 @@ boot.ci(boot_r, index = 7, type = c("norm", "basic", "perc"))
 # bootstrap across all windows, but 
 # exclude windows overlapping inv4m
 boot_r_no_inv4m <- boot(data = filter(d_f4, !is.na(num_sites) & !inv4m), 
-               statistic = calc_cor_r,
-               sim = "ordinary",
-               stype = "i",
-               R = n_boot)
+                        statistic = calc_cor_r,
+                        sim = "ordinary",
+                        stype = "i",
+                        R = n_boot)
 
 # bootstrap confidence interval for spearman's rank correlation:
-boot.ci(boot_r_no_inv4m, index = 1, type = c("norm", "basic", "perc"))
+#boot.ci(boot_r_no_inv4m, index = 1, type = c("norm", "basic", "perc"))
 
 # bootstrap confidence interval for number of low recombination bins:
-boot.ci(boot_r_no_inv4m, index = 11, type = c("norm", "basic", "perc"))
+#boot.ci(boot_r_no_inv4m, index = 11, type = c("norm", "basic", "perc"))
 
 
 # bootstrap sampling within each quintile separately:
@@ -197,9 +199,9 @@ boot.ci(boot_r_no_inv4m, index = 11, type = c("norm", "basic", "perc"))
 calc_cor_r5 <- function(d, i) { # d is data, i is indices
   all = 1:nrow(d)
   j = ifelse(i == all, i, # original sample (not bootstrap) 
-  # ignore original boostrap sample i and use instead new sample j:
-  do.call(c, lapply(1:5, function(x) # resample only within quintiles, not across
-    sample(all[d$quintile_Mbp5 == x], replace = T))))
+             # ignore original boostrap sample i and use instead new sample j:
+             do.call(c, lapply(1:5, function(x) # resample only within quintiles, not across
+               sample(all[d$quintile_Mbp5 == x], replace = T))))
   b = d[j, ] %>%
     group_by(., bin_Mbp5, quintile_Mbp5) %>%
     summarise(f4_num = sum(num_stat)/sum(num_sites),
@@ -212,38 +214,38 @@ calc_cor_r5 <- function(d, i) { # d is data, i is indices
                                   method = "spearman")),
            # also get alpha estimates for each quintile in the bootstrap sample
            unlist(pivot_wider(b, names_from = quintile_Mbp5, 
-                       names_prefix = "Mbp", 
-                       values_from = f4_mex, 
-                       id_cols = quintile_Mbp5))))
+                              names_prefix = "Mbp", 
+                              values_from = f4_mex, 
+                              id_cols = quintile_Mbp5))))
 }
 
 # test:
-filter(d_f4, !is.na(num_sites)) %>%
-  calc_cor_r5(., 1:nrow(.))
-filter(d_f4, !is.na(num_sites) & !inv4m) %>%
-  calc_cor_r5(., 1:nrow(.))
-filter(d_f4, !is.na(num_sites)) %>%
-  calc_cor_r5(., 5)
+#filter(d_f4, !is.na(num_sites)) %>%
+#  calc_cor_r5(., 1:nrow(.))
+#filter(d_f4, !is.na(num_sites) & !inv4m) %>%
+#  calc_cor_r5(., 1:nrow(.))
+#filter(d_f4, !is.na(num_sites)) %>%
+#  calc_cor_r5(., 5)
 
 
 # bootstrap resampling each recombination rate quintile independently
 boot_r5 <- boot(data = filter(d_f4, !is.na(num_sites)), 
-               statistic = calc_cor_r5,
-               sim = "ordinary",
-               stype = "i",
-               R = n_boot)
-
-boot.ci(boot_r5, index = 1, type = c("norm", "basic", "perc"))
-
-# bootstrap resampling each recombination rate quintile independently, BUT
-# excluding all windows overlapping inv4m
-boot_r5_no_inv4m <- boot(data = filter(d_f4, !is.na(num_sites) & !inv4m), 
                 statistic = calc_cor_r5,
                 sim = "ordinary",
                 stype = "i",
                 R = n_boot)
 
-boot.ci(boot_r5_no_inv4m, index = 1, type = c("norm", "basic", "perc"))
+#boot.ci(boot_r5, index = 1, type = c("norm", "basic", "perc"))
+
+# bootstrap resampling each recombination rate quintile independently, BUT
+# excluding all windows overlapping inv4m
+boot_r5_no_inv4m <- boot(data = filter(d_f4, !is.na(num_sites) & !inv4m), 
+                         statistic = calc_cor_r5,
+                         sim = "ordinary",
+                         stype = "i",
+                         R = n_boot)
+
+#boot.ci(boot_r5_no_inv4m, index = 1, type = c("norm", "basic", "perc"))
 
 
 
@@ -269,30 +271,30 @@ calc_cor_cd <- function(d, i) { # d is data, i is indices
 
 
 # test:
-filter(d_f4, !is.na(num_sites)) %>%
-  calc_cor_cd(., 1:nrow(.)) # all windows
-filter(d_f4, !is.na(num_sites) & !inv4m) %>%
-  calc_cor_cd(., 1:nrow(.)) # excluding inv4m
+#filter(d_f4, !is.na(num_sites)) %>%
+#  calc_cor_cd(., 1:nrow(.)) # all windows
+#filter(d_f4, !is.na(num_sites) & !inv4m) %>%
+#  calc_cor_cd(., 1:nrow(.)) # excluding inv4m
 
 # bootstrap of correlation across coding density
 boot_cd <- boot(data = filter(d_f4, !is.na(num_sites)), 
-               statistic = calc_cor_cd,
-               sim = "ordinary",
-               stype = "i",
-               R = n_boot)
-
-# bootstrap ci for spearman's rank correlation:
-boot.ci(boot_cd, index = 1, type = c("norm", "basic", "perc"))
-
-# bootstrap of correlation across coding density
-boot_cd_no_inv4m <- boot(data = filter(d_f4, !is.na(num_sites) & !inv4m), 
                 statistic = calc_cor_cd,
                 sim = "ordinary",
                 stype = "i",
                 R = n_boot)
 
 # bootstrap ci for spearman's rank correlation:
-boot.ci(boot_cd_no_inv4m, index = 1, type = c("norm", "basic", "perc"))
+#boot.ci(boot_cd, index = 1, type = c("norm", "basic", "perc"))
+
+# bootstrap of correlation across coding density
+boot_cd_no_inv4m <- boot(data = filter(d_f4, !is.na(num_sites) & !inv4m), 
+                         statistic = calc_cor_cd,
+                         sim = "ordinary",
+                         stype = "i",
+                         R = n_boot)
+
+# bootstrap ci for spearman's rank correlation:
+#boot.ci(boot_cd_no_inv4m, index = 1, type = c("norm", "basic", "perc"))
 
 
 
@@ -327,7 +329,7 @@ p_r5 <- ggplot(data = d_boot_r, aes(x = mbp_bin)) +
   # and errorbars for 90% CI around that mean
   # based on bootstrap with NGSAdmix
   geom_errorbar(data = ci_boot_r, aes(ymin = low,
-                    ymax = high),
+                                      ymax = high),
                 width = .5) +
   xlab("Mbp per cM (quintiles low -> high)") +
   ylab("Proportion mexicana ancestry") +
@@ -357,8 +359,8 @@ ci_boot_r_no_inv4m <- t(sapply(1:5, function(x)
   mutate(mbp_bin = paste0("Mbp", 1:5))
 ci_spearman_r_no_inv4m = boot.ci(boot_r_no_inv4m, index = 1, conf = 0.95, type = "basic")
 text_spearman_r_no_inv4m = paste0("Spearman's rho = ", ci_spearman_r_no_inv4m$t0, 
-                         " CI_95%(", ci_spearman_r_no_inv4m$basic[4], ", ",
-                         ci_spearman_r_no_inv4m$basic[5], ")")
+                                  " CI_95%(", ci_spearman_r_no_inv4m$basic[4], ", ",
+                                  ci_spearman_r_no_inv4m$basic[5], ")")
 
 p_r5_no_inv4m <- ggplot(data = d_boot_r_no_inv4m, aes(x = mbp_bin)) +
   # first plot original point estimates for ind. ancestry
@@ -374,7 +376,7 @@ p_r5_no_inv4m <- ggplot(data = d_boot_r_no_inv4m, aes(x = mbp_bin)) +
   # and errorbars for 90% CI around that mean
   # based on bootstrap with NGSAdmix
   geom_errorbar(data = ci_boot_r_no_inv4m, aes(ymin = low,
-                                      ymax = high),
+                                               ymax = high),
                 width = .5) +
   xlab("Mbp per cM (quintiles low -> high)") +
   ylab("Proportion mexicana ancestry") +
@@ -405,8 +407,8 @@ ci_boot_cd <- t(sapply(1:5, function(x)
   mutate(cd_bin = paste0("cd", 1:5))
 ci_spearman_cd = boot.ci(boot_cd, index = 1, conf = 0.95, type = "basic")
 text_spearman_cd = paste0("Spearman's rho = ", ci_spearman_cd$t0, 
-                         " CI_95%(", ci_spearman_cd$basic[4], ", ",
-                         ci_spearman_cd$basic[5], ")")
+                          " CI_95%(", ci_spearman_cd$basic[4], ", ",
+                          ci_spearman_cd$basic[5], ")")
 
 p_cd5 <- ggplot(data = d_boot_cd, aes(x = cd_bin)) +
   # first plot original point estimates for ind. ancestry
@@ -422,7 +424,7 @@ p_cd5 <- ggplot(data = d_boot_cd, aes(x = cd_bin)) +
   # and errorbars for 90% CI around that mean
   # based on bootstrap with NGSAdmix
   geom_errorbar(data = ci_boot_cd, aes(ymin = low,
-                                      ymax = high),
+                                       ymax = high),
                 width = .5) +
   xlab("Coding bp per cM (quintiles low -> high)") +
   ylab("Proportion mexicana ancestry") +
@@ -453,8 +455,8 @@ ci_boot_cd_no_inv4m <- t(sapply(1:5, function(x)
   mutate(cd_bin = paste0("cd", 1:5))
 ci_spearman_cd_no_inv4m = boot.ci(boot_cd_no_inv4m, index = 1, conf = 0.95, type = "basic")
 text_spearman_cd_no_inv4m = paste0("Spearman's rho = ", ci_spearman_cd_no_inv4m$t0, 
-                          " CI_95%(", ci_spearman_cd_no_inv4m$basic[4], ", ",
-                          ci_spearman_cd_no_inv4m$basic[5], ")")
+                                   " CI_95%(", ci_spearman_cd_no_inv4m$basic[4], ", ",
+                                   ci_spearman_cd_no_inv4m$basic[5], ")")
 
 p_cd5_no_inv4m <- ggplot(data = d_boot_cd_no_inv4m, aes(x = cd_bin)) +
   # first plot original point estimates for ind. ancestry
@@ -470,7 +472,7 @@ p_cd5_no_inv4m <- ggplot(data = d_boot_cd_no_inv4m, aes(x = cd_bin)) +
   # and errorbars for 90% CI around that mean
   # based on bootstrap with NGSAdmix
   geom_errorbar(data = ci_boot_cd_no_inv4m, aes(ymin = low,
-                                       ymax = high),
+                                                ymax = high),
                 width = .5) +
   xlab("Coding bp per cM (quintiles low -> high)") +
   ylab("Proportion mexicana ancestry") +
