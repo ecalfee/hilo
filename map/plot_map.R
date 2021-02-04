07bdc522db4903d5dd0f02fc8f0d9d55504d5bc8
Erin Calfee <erincalfee@gmail.com>
2021-02-03 17:08:17 -0800

    first pass map of sampling sites compared to teosinte elevational range

--- map/plot_map.R
+++ map/plot_map.R
@@ -10,12 +10,19 @@ library(gridExtra)
 library(readxl)
 library(cowplot)
 library(scales)
+library(geodist)
 
 
 # load variables from snakemake
 # get output plot filenames
 png_map = snakemake@output[["png_map"]]
 # png_map = "map/plots/mexico_map_elev.png"
+png_teo_hist = snakemake@output[["png_teo_hist"]]
+# png_teo_hist = "map/plots/teosinte_hist.png"
+png_map_teo_color = snakemake@output[["png_map_teo_color"]]
+# png_map_teo_color = "map/plots/mexico_lines_elev_teo_color.png"
+png_map_teo_black = snakemake@output[["png_map_teo_black"]]
+# png_map_teo_black = "map/plots/mexico_lines_elev_teo_black.png"
 
 # get colors for plot
 source(snakemake@input[["colors"]])
@@ -50,7 +57,7 @@ mexico <- map_data("world")  %>% # world data, country outlines
 # function to scale elevation of pops to a bar across lat
 bar_x = c(-115, -90)
 bar_y = c(15, 32.5)
-bar_teo = c(0, 3050)
+bar_teo = c(-0, 3050)
 get_scaled_xy_pos <- function(elev, bar_xy, bar_teo){
   length_xy = bar_xy[2] - bar_xy[1]
   length_teo = bar_teo[2] - bar_teo[1]
@@ -60,6 +67,8 @@ get_scaled_xy_pos <- function(elev, bar_xy, bar_teo){
 
 pop_to_map <- filter(meta, group == "sympatric_maize") %>%
   arrange(desc(ELEVATION)) %>%
+  dplyr::select("LOCALITY", "ELEVATION", "LAT", "LON") %>%
+  filter(!duplicated(.)) %>%
   mutate(LOCALITY_ELEVATION = factor(paste0(LOCALITY, " (", ELEVATION, "m)"), 
                                      ordered = T, 
                                      levels = unique(paste0(.$LOCALITY, " (", .$ELEVATION, "m)"))),
@@ -70,196 +79,163 @@ pop_to_map <- filter(meta, group == "sympatric_maize") %>%
                                    bar_xy = bar_y,
                                    bar_teo = bar_teo))
 
-# make map
-p_map <-  pop_to_map %>%
-  ggplot(data = .) +
-  geom_polygon(data = mexico,
-    aes(x = long, y = lat, group = group), 
-               fill = "lightgrey") +
-  #coord_cartesian() +
-  coord_equal(#xlim = c(-110, -90)
-    xlim = bar_x,
-    ylim = bar_y,
-                ) +
-  theme_classic() +
-  geom_point(
-    aes(x = LON,
-        y = LAT,
-        shape = LOCALITY_ELEVATION,
-        color = LOCALITY_ELEVATION),
-             size = 2,
-             alpha = 1) +
-  xlab("Longitude") +
-  ylab("Latitude") +
-  scale_color_viridis_d(direction = 1, option = "viridis") +
-  labs(color = NULL, shape = NULL) +
-  #scale_shape_manual(values = rep(c(15, 17, 19, 0, 2, 1), 3)) +
-  #scale_shape_manual(values = rep(c(1, 17, 0, 19, 2, 15), 3))
-  #scale_shape_manual(values = rep(c(1, 0, 2, 4), 5))
-  scale_shape_manual(values = rep(c(2, 0, 3, 1, 6, 5), 5))
-p_map
 
-ggsave(filename = png_map,
-       plot = p_map,
-       device = "png", height = 4, width = 7.5, units = "in", dpi = 300)
-
-# add scale bar and lines
-p_map_lines <- pop_to_map %>%
+# make a map with scale bar and lines
+p_map <- pop_to_map %>%
   ggplot(data = .) +
   geom_polygon(data = mexico,
                aes(x = long, y = lat, group = group), 
                fill = "lightgrey") +
-  coord_equal(
-    xlim = bar_x,
-    ylim = bar_y
-  ) +
-  scale_y_continuous(expand = c(0,0), 
-                     #limits = bar_y, 
-                     #labels = scales::number_format(accuracy = 0.0)
-                     ) +
-  scale_x_continuous(expand = c(0,0), 
-                     #limits = bar_x, 
-                     #labels = scales::number_format(accuracy = 0.0)
-                     ) +
+  coord_equal(xlim = bar_x, ylim = bar_y) +
+  scale_y_continuous(expand = c(0,0)) +
+  scale_x_continuous(expand = c(0,0)) +
   theme_classic() +
   geom_point(
     aes(x = LON,
         y = LAT,
-        color = ELEVATION),
-    size = .1) +
+        color = LOCALITY_ELEVATION),
+    size = 2) +
   xlab("Longitude") +
   ylab("Latitude") +
-  scale_color_viridis(direction = -1, option = "viridis") +
-  geom_segment(aes(x = LON, y = LAT, xend = pos_x,
-                   color = ELEVATION), yend = bar_y[2]) +
-  labs(color = "Elevation (m)")
-p_map_lines
-
-# how big do I have to make it to see all the points in mexico?
-zoom_map <- pop_to_map %>%
-  ggplot(data = .) +
-  #geom_polygon(aes(x = long, y = lat, group = group), 
-  #             fill = "lightgrey") +
-  #coord_cartesian() +
-  coord_equal(#xlim = c(-110, -95),
-              #ylim = c(15, 28)
-    xlim = c(-102.5, -98.2),
-    ylim = c(18.8, 20.3)
-    ) +
-  theme_classic() +
-  geom_point(aes(x = LON,
-                 y = LAT,
-                 shape = LOCALITY_ELEVATION,
-                 color = LOCALITY_ELEVATION),
-             size = 2) +
-  xlab("Longitude") +
-  ylab("Latitude") +
-  #scale_shape_manual(values = rep(c(15, 17, 19, 0, 2, 1), 3)) +
-  scale_shape_manual(values = rep(c(2, 0, 3, 1, 6, 5), 5)) +
   scale_color_viridis_d(direction = 1, option = "viridis") +
-  labs(shape = F, color = F)
-# try adding greyscale elevation underneath?
-# can you make a pop-out square for samples closer together?
-zoom_map
-ggsave(filename = "map/plots/zoom_cluster_sampling_locations.png",
-       plot = zoom_map,
-       device = "png", 
-       height = 4, width = 7.5, 
-       units = "in", dpi = 300)
+  guides(color = guide_legend(title = "Maize/Mexicana Sites", 
+                       override.aes = list(linetype = 0, size = 2)))
+#p_map
+
+p_map_lines_color = p_map + 
+  geom_segment(aes(x = LON, y = LAT, xend = pos_x,
+                   color = LOCALITY_ELEVATION), yend = bar_y[2])
+p_map_lines_black = p_map + 
+  geom_segment(aes(x = LON, y = LAT, xend = pos_x),
+                   color = "black", yend = bar_y[2]) +
+  geom_point(
+    aes(x = LON,
+        y = LAT,
+        color = LOCALITY_ELEVATION),
+    size = 2)# to add on top of lines
 
 # histogram of teosinte occurence data
 p_teo_hist <- teo %>%
   ggplot(.) +
   geom_histogram(aes(x = ELEVATION, fill = zea),
-                 alpha = 0.5, 
-                 bins = 35,
+                 alpha = 0.75,
+                 binwidth = 100,
                  position = "identity") +
   theme_classic() +
-  scale_x_continuous(position = "top", limits = bar_teo, expand = c(0,0)) +
-  scale_y_continuous(#expand = c(0,0), 
-                     #limits = c(0, 125), 
-                     breaks = c(0, 30, 60, 90)) +
+  scale_x_continuous(position = "top", 
+                     limits = bar_teo, # 2 missing rows are ok (this is for empty histogram bins from (-50,50) with zero height). 
+                     # you can see this by changing limits to c(-50,3050) and the ggplot2 warning goes away 
+                     expand = c(0, 0)
+                     ) +
   scale_fill_manual(values = col_maize_mex_parv) +
   ggtitle("Teosinte occurence data 1842–2016") +
   labs(fill = "Teosinte", x = "Elevation (m)", y = "Observations")
-# plot(p_teo_hist)
+  
+# p_teo_hist
 ggsave(filename = png_teo_hist,
        plot = p_teo_hist,
        device = "png", 
        height = 3, width = 5, 
        units = "in", dpi = 300)
 
-# p_map_lines, p_teo_hist
-
-p_combined <- grid.arrange(grobs = list(
+# make combined map and histogram
+p_combined_color <- grid.arrange(grobs = list(
   ggplotGrob(p_teo_hist + 
                geom_point(data = pop_to_map,
                           aes(x = ELEVATION,
-                              color = ELEVATION),
+                              color = LOCALITY_ELEVATION),
                           y = 0,
                           shape = 3) +
-               scale_color_viridis(direction = -1, option = "viridis") +
+               geom_hline(yintercept = 0) +
+               scale_color_viridis_d(direction = 1, option = "viridis") +
+               theme(plot.title = element_blank(),
+                     legend.position = "None",
+                     plot.margin = margin(t = 5, r = 5, b = 0, l = 5, unit = "pt")) +
+               geom_segment(x = 0, 
+                            y = 118, 
+                            xend = 3050, 
+                            yend = 118,
+                            color = col_maize_mex_parv[["maize"]],
+                            arrow = arrow(length = unit(1, "mm"), 
+                                          ends = "last",
+                                          type = "closed")) +
+               geom_text(x = 2700, 
+                         y = 102, 
+                         color = col_maize_mex_parv[["maize"]],
+                         label = "maize range",
+                         size = 3)
+    ),
+  ggplotGrob(p_map_lines_color + 
+               theme(legend.position = "None",
+                     plot.margin = margin(t = 0, r = 5, b = 5, l = 5, unit = "pt"))),
+  cowplot::get_legend(p_teo_hist + 
+                        theme(legend.position = "left")),
+  cowplot::get_legend(p_map),
+  textGrob(label = "A", 
+           just = "top",
+           x = unit(0.5, "lines"), 
+           y = unit(0, "lines")),
+  textGrob(label = "B", 
+           just = "top",
+           x = unit(0.5, "lines"), 
+           y = unit(0, "lines"))
+  ),
+  layout_matrix = rbind(
+    c(5, 1,3),
+    c(6, 2,4)),
+  heights = c(2, 5),
+  widths = c(.1, 5, 2))
+
+p_combined_color
+ggsave(png_map_and_teo_color, 
+       plot = p_combined_color, 
+       device = "png", 
+       width = 7.1,#5, 
+       height = 5,#5, 
+       units = "in",
+       dpi = 300)
+
+p_combined_black <- grid.arrange(grobs = list(
+  ggplotGrob(p_teo_hist + 
+               geom_point(data = pop_to_map,
+                          aes(x = ELEVATION),
+                              color = "black",
+                          y = 0,
+                          shape = 3) +
+               geom_hline(yintercept = 0) +
                theme(plot.title = element_blank(),
                      legend.position = "None",
                      plot.margin = margin(t = 5, r = 5, b = 0, l = 5, unit = "pt"))
-    ),
-  ggplotGrob(p_map_lines + 
+  ),
+  ggplotGrob(p_map_lines_black + 
                theme(legend.position = "None",
                      plot.margin = margin(t = 0, r = 5, b = 5, l = 5, unit = "pt"))),
-  cowplot::get_legend(p_teo_hist + theme(legend.position = "left")), # + labs(fill = "Historical Occurrence")),
-  cowplot::get_legend(p_map + labs(color = "Sympatric sites", #element_blank(),
-                                   shape = "Sympatric sites") + #element_blank()) + # Sympatric sites?
-                        guides(color = guide_legend(override.aes = list(shape = 16)))) 
-  ),
-  layout_matrix = rbind(
-    c(1,3),
-    c(2,4)),
-  heights = c(2, 5),
-  widths = c(5, 2))
+  cowplot::get_legend(p_teo_hist + 
+                        theme(legend.position = "left")),
+  cowplot::get_legend(p_map) 
+),
+layout_matrix = rbind(
+  c(1,3),
+  c(2,4)),
+heights = c(2, 5),
+widths = c(5, 2))
 
-p_combined
-png_map_and_teo = "map/plots/mexico_lines_elev_teo2.png"
-ggsave(png_map_and_teo, 
-       plot = p_combined, 
+p_combined_black
+png_map_and_teo_black = "map/plots/mexico_lines_elev_teo_black.png"
+ggsave(png_map_and_teo_black, 
+       plot = p_combined_black, 
        device = "png", 
        width = 7,#5, 
        height = 5,#5, 
        units = "in",
        dpi = 300)
 
-p_combined2 = cowplot::plot_grid(ggplotGrob(p_teo_hist + 
-                                geom_point(data = pop_to_map,
-                                           aes(x = ELEVATION,
-                                               color = ELEVATION),
-                                           y = 0,
-                                           shape = 3) +
-                                scale_color_viridis(direction = -1, option = "viridis") +
-                                theme(plot.title = element_blank(),
-                                      legend.position = "None",
-                                      plot.margin = margin(t = 5, r = 5, b = 0, l = 5, unit = "pt"))
-), 
-ggplotGrob(p_map_lines + 
-             theme(legend.position = "None",
-                   plot.margin = margin(t = 0, r = 5, b = 5, l = 5, unit = "pt"))), 
-                   
-ncol = 1, 
-align = "v")
-p_combined2
-p_combined
-png_map_and_teo = "map/plots/mexico_lines_elev_teo.png"
-ggsave(png_map_and_teo, 
-       plot = p_combined, 
-       device = "png", 
-       width = 5, 
-       height = 5, 
-       units = "in",
-       dpi = 300)
-
 
 # ---------- geographic distance -------------- #
+# plot heatmap of geographic distance between populations
+
 # get geographic distances between populations!
-all_places <- meta %>%
-  filter(group == "sympatric_maize") %>%
+all_places <- pop_to_map %>%
   arrange(., desc(ELEVATION)) %>%
   .$LOCALITY
 edges_geo <- data.frame(from = unlist(lapply(1:length(all_places), 
@@ -270,10 +246,10 @@ edges_geo <- data.frame(from = unlist(lapply(1:length(all_places),
                                              all_places[i:length(all_places)])),
                         stringsAsFactors = F) %>% # list all unique pairs of locations
   left_join(., 
-            dplyr::select(meta_pops_list[["maize"]], LOCALITY, LAT, LON), 
+            dplyr::select(pop_to_map, LOCALITY, LAT, LON), 
             by = c("from"="LOCALITY")) %>%
   left_join(., 
-            dplyr::select(meta_pops_list[["maize"]], LOCALITY, LAT, LON), 
+            dplyr::select(pop_to_map, LOCALITY, LAT, LON), 
             by = c("to"="LOCALITY"),
             suffix = c(".from", ".to")) %>%
   # calculate pairwise geodesic distance between locations (in m)
@@ -284,6 +260,24 @@ edges_geo <- data.frame(from = unlist(lapply(1:length(all_places),
   dplyr::mutate(distance_km = distance/1000) %>%
   dplyr::mutate(distance_km_truncated = ifelse(distance_km > 500, 500, distance_km)) %>%
   dplyr::select(from, to, distance_km, distance_km_truncated)
+
+distances <- bind_rows(edges_geo,
+          edges_geo %>%
+            mutate(from_old = from,
+                   from = to,
+                   to = from_old)) %>%
+  mutate(from = factor(from, ordered = T, levels = all_places[length(all_places):1]),
+         to = factor(to, ordered = T, levels = all_places[length(all_places):1]))
+
+dist_heatmap <- distances %>% # add 2nd half of pairs (reversed to <-> from)
+  ggplot(aes(x = from, y = to, fill = distance_km_truncated)) +
+  geom_tile() +
+  theme_classic() +
+  scale_fill_viridis()
+
+
+
+
 
 net_tidy_geo <- tbl_graph(nodes = nodes, 
                           edges = edges_geo,
