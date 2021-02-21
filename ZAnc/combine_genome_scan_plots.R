18369f83581a83e871e7d085231727011da8c017
Erin Calfee <erincalfee@gmail.com>
2021-02-20 16:06:15 -0800

    fixed plot label switch on 4 genome scan multi plot

--- ZAnc/combine_genome_scan_plots.R
+++ ZAnc/combine_genome_scan_plots.R
@@ -84,9 +84,9 @@ p_multi <- grid.arrange(grobs = list(textGrob(label = "A",
                                               just = "center",
                                               gp = gpar(fontsize = 11)),
                                      cowplot::get_legend(plot_for_legend_only),
-                                     textGrob(label = "slope mexicana ancestry ~ elevation",
-                                              rot = 90),
                                      textGrob(label = "mean mexicana ancestry",
+                                              rot = 90),
+                                     textGrob(label = "slope mexicana ancestry ~ elevation",
                                               rot = 90)
                                      ),
                         layout_matrix = rbind(
