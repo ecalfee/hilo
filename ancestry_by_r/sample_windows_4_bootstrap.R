d6a6164f28a24689cd598cd498ceaf2802264a62
Erin Calfee <erincalfee@gmail.com>
2021-04-08 11:32:45 -0700

    generalized to different prefixes upstream of ngsadmix bootstrap

--- ancestry_by_r/sample_windows_4_bootstrap.R
+++ ancestry_by_r/sample_windows_4_bootstrap.R
@@ -5,23 +5,26 @@ library(tidyr)
 # load variables from Snakefile
 windows = snakemake@input[["windows"]]
 # windows = "results/map_pos_1cM_windows.txt"
-prefix_all = snakemake@params[["prefix_all"]]
-# prefix_all = "HILO_MAIZE55"
+prefix = snakemake@params[["prefix"]]
+# prefix = "HILO_MAIZE55"
 n = as.integer(snakemake@params[["n"]]) # number of bootstraps
 # n = 100
 feature = snakemake@params[["feature"]]
+# feature = "r"
+seed = snakemake@params[["seed"]]
+# seed = 100
 
 map_pos_1cM <- read.table(windows, header = T, sep = "\t", stringsAsFactors = F)
 
 quintile = paste0("quintile_", feature, 5) # e.g. quintile_r5
 
 # make n bootstrap replicate lists of windows:
-set.seed(100) # set seed for replication
+set.seed(seed) # set seed for replication
 for (r in 1:5){ # 5 recombination rate quintiles
   r_windows <- map_pos_1cM$window[map_pos_1cM[ , quintile] == r]
   # boot0 is the original data/windows:
   write.table(x = r_windows,
-              file = paste0("ancestry_by_r/results/bootstrap_1cM/", prefix_all, "/", feature, "5_", r, "/boot", 0, ".list"),
+              file = paste0("ancestry_by_r/results/bootstrap_1cM/", prefix, "/", feature, "5_", r, "/boot", 0, ".list"),
               quote = F, col.names = F, row.names = F, sep = "\t")
   for (b in 1:n){ # 100 bootstrap samples
     # (same size = same # windows as original data
@@ -29,7 +32,7 @@ for (r in 1:5){ # 5 recombination rate quintiles
     write.table(x = sample(x = r_windows,
                          size = length(r_windows),
                          replace = T),
-                file = paste0("ancestry_by_r/results/bootstrap_1cM/", prefix_all, "/", feature, "5_", r, "/boot", b, ".list"),
+                file = paste0("ancestry_by_r/results/bootstrap_1cM/", prefix, "/", feature, "5_", r, "/boot", b, ".list"),
                 quote = F, col.names = F, row.names = F, sep = "\t")
   }
 }
