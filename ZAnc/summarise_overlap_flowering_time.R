775371cf028e7c0f356debd3ec96e57789c037b7
Erin Calfee <erincalfee@gmail.com>
2021-06-02 08:44:58 -0700

    updated input file names in summarise flowering time R script

--- ZAnc/summarise_overlap_flowering_time.R
+++ ZAnc/summarise_overlap_flowering_time.R
@@ -6,19 +6,21 @@ Ne = snakemake@params[["Ne"]]
 # Ne = 10000
 yesno = snakemake@params[["yesno"]]
 # yesno = "yes"
-prefix_all = snakemake@params[["prefix"]]
-# prefix_all = "HILO_MAIZE55"
+prefix = snakemake@params[["prefix"]]
+# prefix = "HILO_MAIZE55"
+K = snakemake@params[["K"]]
+# K = 2
 txt_out = snakemake@output[["txt"]] # txt output file
-# txt_out = paste0("ZAnc/results/", prefix_all, "/Ne", Ne, "_", yesno, "Boot/flowering_time_genes_v4.plus20kb.overlap.summary_overlap_outliers.txt")
+# txt_out = paste0("ZAnc/results/", prefix, "/K", K, "/Ne", Ne, "_", yesno, "Boot/flowering_time_genes_v4.plus20kb.overlap.summary_overlap_outliers.txt")
 
 mex_maize = c("mexicana", "maize")
 posneg = c("pos", "neg")
-stats = c("meanAnc", "lmElev")
+stats = c("mexicana_anc", "lmElev")
 sigs = c("fdr05", "perc05", "p05")
 
 # function to calculate p-values for overlap based on permutation test
-calc_pval = function(zea, sign, stat, sig, prefix_all, Ne, yesno){
-  results_file = paste0("ZAnc/results/", prefix_all, "/Ne", Ne, "_", yesno, 
+calc_pval = function(zea, sign, stat, sig, prefix, K, Ne, yesno){
+  results_file = paste0("ZAnc/results/", prefix, "/K", K, "/Ne", Ne, "_", yesno, 
                         "Boot/flowering_time_genes_v4.plus20kb.", zea, "_", sign,
                         "_", stat, "_outliers.", sig, ".counts")
   count = read.table(results_file, header = F, nrows = 1)[1,1]
@@ -33,6 +35,6 @@ all_combs = expand.grid(zea = mex_maize, sign = posneg, stat = stats, sig = sigs
 results = do.call(rbind,
                   apply(all_combs, 1, function(row) 
   calc_pval(zea = row[["zea"]], sign = row[["sign"]], stat = row[["stat"]], sig = row[["sig"]], 
-                  prefix_all = prefix_all, Ne = Ne, yesno = yesno)))
+                  prefix = prefix, Ne = Ne, yesno = yesno)))
 
 write.table(results, txt_out, sep = "\t", col.names = T, row.names = F, quote = F)
